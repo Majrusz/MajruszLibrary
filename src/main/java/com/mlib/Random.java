5c3efdbcacf3d0265d6b88ab457de4be07475c94
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-23 17:28:29 +0100

    New methods for vectors

--- src/main/java/com/mlib/Random.java
+++ src/main/java/com/mlib/Random.java
@@ -1,5 +1,7 @@
 package com.mlib;
 
+import net.minecraft.util.math.vector.Vector3d;
+
 public class Random {
 	/**
 	 The method responsible for the random event.
@@ -10,5 +12,24 @@ public class Random {
 	 */
 	public static boolean tryChance( double chance ) {
 		return MajruszLibrary.RANDOM.nextDouble() <= chance;
+	}
+
+	/** Returns random vector 3d. */
+	public static Vector3d getRandomVector3d( double minX, double maxX, double minY, double maxY, double minZ, double maxZ ) {
+		double x = ( maxX - minX ) * MajruszLibrary.RANDOM.nextDouble() + minX;
+		double y = ( maxY - minY ) * MajruszLibrary.RANDOM.nextDouble() + minY;
+		double z = ( maxZ - minZ ) * MajruszLibrary.RANDOM.nextDouble() + minZ;
+
+		return new Vector3d( x, y, z );
+	}
+
+	/** Returns random normalized vector 3d. (with length = 1) */
+	public static Vector3d getRandomNormalizedVector3d() {
+		Vector3d vector = getRandomVector3d( -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 );
+
+		if( vector.length() < 1e-6 ) // to avoid dividing by zero (or very small number) and throwing exception
+			return getRandomNormalizedVector3d();
+		else
+			return vector.normalize();
 	}
 }
