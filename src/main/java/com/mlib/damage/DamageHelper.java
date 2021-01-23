703c815b6ba2fa00d9fbde43561efec4bea1018d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-23 17:38:14 +0100

    Added isDirectDamageFromLivingEntity method

--- src/main/java/com/mlib/damage/DamageHelper.java
+++ src/main/java/com/mlib/damage/DamageHelper.java
@@ -1,5 +1,6 @@
 package com.mlib.damage;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.DamageSource;
 
@@ -14,5 +15,13 @@ public class DamageHelper {
 	@Nullable
 	public static PlayerEntity getPlayerFromDamageSource( DamageSource damageSource ) {
 		return damageSource.getTrueSource() instanceof PlayerEntity ? ( PlayerEntity )damageSource.getTrueSource() : null;
+	}
+
+	/**
+	 Checks whether damage source from directly from living entity.
+	 For example if skeleton shoots an arrow then it is not direct damage.
+	 */
+	public static boolean isDirectDamageFromLivingEntity( DamageSource source ) {
+		return source.getTrueSource() instanceof LivingEntity && source.getImmediateSource() instanceof LivingEntity;
 	}
 }
