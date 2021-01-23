8233de9abe354d0f6d7d88a6fcbea3d97b1066d7
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-23 17:06:18 +0100

    New method for checking if entity is outside when it is raining

--- src/main/java/com/mlib/WorldHelper.java
+++ src/main/java/com/mlib/WorldHelper.java
@@ -1,10 +1,13 @@
 package com.mlib;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.server.ServerWorld;
 
 /** Simple class for more explicit way to handling world functions. */
 public class WorldHelper {
@@ -42,5 +45,18 @@ public class WorldHelper {
 		DifficultyInstance difficultyInstance = getDifficultyInstance( entity );
 
 		return difficultyInstance.getClampedAdditionalDifficulty();
+	}
+
+	/**
+	 Checks whether entity is outside when it is raining.
+
+	 @param entity Entity to check.
+	 */
+	public static boolean isEntityOutsideWhenItIsRaining( Entity entity ) {
+		World world = entity.world;
+		BlockPos entityPosition = new BlockPos( entity.getPositionVec() );
+		Biome biome = world.getBiome( entityPosition );
+
+		return world.canSeeSky( entityPosition ) && world.isRaining() && biome.getPrecipitation() == Biome.RainType.RAIN;
 	}
 }
