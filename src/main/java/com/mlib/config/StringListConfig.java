c0af7810f5c405a4dc62f78c822a58ee068a5468
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-03-29 14:00:47 +0200

    Added new constructor to String List Config

--- src/main/java/com/mlib/config/StringListConfig.java
+++ src/main/java/com/mlib/config/StringListConfig.java
@@ -3,23 +3,25 @@ package com.mlib.config;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /** Class representing list of strings config. */
 public class StringListConfig extends BaseConfig {
-	public ForgeConfigSpec.ConfigValue< List< ? extends String > > listValues;
 	private final List< ? extends String > defaultValues;
+	public ForgeConfigSpec.ConfigValue< List< ? extends String > > listValues;
 
 	public StringListConfig( String name, String comment, boolean requiresWorldRestart, List< ? extends String > defaultValues ) {
 		super( name, comment, requiresWorldRestart );
 		this.defaultValues = defaultValues;
 	}
 
-	/** Checks if a list contains given value. */
-	public boolean hasValue( String value ) {
-		List< ? extends String > values = this.listValues.get();
-
-		return values.contains( value );
+	public StringListConfig( String name, String comment, boolean requiresWorldRestart, String... defaultElements ) {
+		super( name, comment, requiresWorldRestart );
+		List< String > defaultList = new ArrayList<>();
+		Collections.addAll( defaultList, defaultElements );
+		
+		this.defaultValues = defaultList;
 	}
 
 	/** Builds current config. */
@@ -28,5 +30,12 @@ public class StringListConfig extends BaseConfig {
 		super.build( builder );
 
 		this.listValues = builder.defineList( this.name, this.defaultValues, x->true );
+	}
+
+	/** Checks if a list contains given value. */
+	public boolean hasValue( String value ) {
+		List< ? extends String > values = this.listValues.get();
+
+		return values.contains( value );
 	}
 }
