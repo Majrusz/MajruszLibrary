06f67af689a2ac21221d742eda4a80cd8170b2c6
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-06-26 15:24:19 +0200

    Updated version name

--- src/main/java/com/mlib/MajruszLibrary.java
+++ src/main/java/com/mlib/MajruszLibrary.java
@@ -17,7 +17,7 @@ import java.util.Random;
 public class MajruszLibrary {
 	public static final String MOD_ID = "majrusz_library";
 	public static final String NAME = "Majrusz Library";
-	public static final String VERSION = "2.0.0";
+	public static final String VERSION = "2.0.1";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final Random RANDOM = new Random();
 
