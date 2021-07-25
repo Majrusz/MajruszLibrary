df54e0dbc55c0c618642c199a565dd810a01652d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-25 17:18:57 +0200

    Renamed EquipmentSlotTypes to EquipmentSlots and updated to 1.17.1

---
+++ src/main/java/com/mlib/EquipmentSlots.java
@@ -0,0 +1,14 @@
+package com.mlib;
+
+import net.minecraft.world.entity.EquipmentSlot;
+
+/** Equipment slot types grouped together. */
+public class EquipmentSlots {
+	public static EquipmentSlot[] ARMOR = new EquipmentSlot[]{ EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS,
+		EquipmentSlot.FEET
+	};
+	public static EquipmentSlot[] BOTH_HANDS = new EquipmentSlot[]{ EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND };
+	public static EquipmentSlot[] ARMOR_AND_HANDS = new EquipmentSlot[]{ EquipmentSlot.HEAD, EquipmentSlot.CHEST,
+		EquipmentSlot.LEGS, EquipmentSlot.FEET, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND
+	};
+}
