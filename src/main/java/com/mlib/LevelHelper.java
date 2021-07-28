85ef7568385eb334b5f0e193e528a8517571155b
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-28 21:42:24 +0200

    Added function for freezing water

--- src/main/java/com/mlib/LevelHelper.java
+++ src/main/java/com/mlib/LevelHelper.java
@@ -4,11 +4,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
 
 import javax.annotation.Nullable;
 import java.util.Optional;
@@ -123,5 +130,41 @@ public class LevelHelper {
 		}
 
 		return spawnPosition.orElseGet( level::getSharedSpawnPos );
+	}
+
+	/** Freeze water under given entity. */
+	public static void freezeWater( LivingEntity entity, double radius, int minimumIceDuration, int maximumIceDuration ) {
+		if( !entity.isOnGround() )
+			return;
+
+		BlockState iceBlockState = Blocks.FROSTED_ICE.defaultBlockState();
+		BlockPos entityPosition = entity.getOnPos();
+		BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
+		Iterable< BlockPos > blocksInRange = BlockPos.betweenClosed( entityPosition.offset( -radius, -1.0, -radius ),
+			entityPosition.offset( radius, -1.0, radius )
+		);
+
+		for( BlockPos blockPos : blocksInRange ) {
+			if( blockPos.closerThan( entityPosition, radius ) ) {
+				mutableBlockPos.set( blockPos.getX(), blockPos.getY() + 1, blockPos.getZ() );
+				BlockState blockState = entity.level.getBlockState( mutableBlockPos );
+				if( blockState.isAir() ) {
+					BlockState blockState2 = entity.level.getBlockState( blockPos );
+					boolean isWater = blockState2.getMaterial() == Material.WATER;
+					boolean isFull = blockState2.getBlock() == Blocks.WATER && blockState2.getValue( LiquidBlock.LEVEL ) == 0;
+					boolean canSurvive = iceBlockState.canSurvive( entity.level, blockPos );
+					boolean isUnobstructed = entity.level.isUnobstructed( iceBlockState, blockPos, CollisionContext.empty() );
+
+					if( isWater && isFull && canSurvive && isUnobstructed && !net.minecraftforge.event.ForgeEventFactory.onBlockPlace( entity,
+						net.minecraftforge.common.util.BlockSnapshot.create( entity.level.dimension(), entity.level, blockPos ),
+						net.minecraft.core.Direction.UP
+					) ) {
+						entity.level.setBlockAndUpdate( blockPos, iceBlockState );
+						entity.level.getBlockTicks()
+							.scheduleTick( blockPos, Blocks.FROSTED_ICE, Mth.nextInt( entity.getRandom(), minimumIceDuration, maximumIceDuration ) );
+					}
+				}
+			}
+		}
 	}
 }
