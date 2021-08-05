da44613c7ba91b8edb10346720028e83159bef53
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-08-05 21:47:06 +0200

    Added getPlayerFromEntity function

--- src/main/java/com/mlib/entities/EntityHelper.java
+++ src/main/java/com/mlib/entities/EntityHelper.java
@@ -4,7 +4,11 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+
+import javax.annotation.Nullable;
 
 /** Class with common code for entities. */
 public class EntityHelper {
@@ -18,5 +22,14 @@ public class EntityHelper {
 			level.sendParticles( ParticleTypes.TOTEM_OF_UNDYING, entity.getX(), entity.getY( 0.75 ), entity.getZ(), 64, 0.25, 0.5, 0.25, 0.5 );
 			level.playSound( null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.TOTEM_USE, SoundSource.AMBIENT, 1.0f, 1.0f );
 		}
+	}
+
+	/**
+	 Returns Player from given entity.
+	 Returns null if casting was impossible.
+	 */
+	@Nullable
+	public static Player getPlayerFromEntity( @Nullable Entity entity ) {
+		return entity instanceof Player ? ( Player )entity : null;
 	}
 }
