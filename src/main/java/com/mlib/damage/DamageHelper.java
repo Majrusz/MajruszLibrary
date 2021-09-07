7ce7167e54e2b645073f4214153c32d03f91427e
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-07 19:21:14 +0200

    Update DamageHelper.java

--- src/main/java/com/mlib/damage/DamageHelper.java
+++ src/main/java/com/mlib/damage/DamageHelper.java
@@ -10,8 +10,28 @@ import javax.annotation.Nullable;
 
 /** Methods for easier damage and damage source handling. */
 public class DamageHelper {
+	/** Tries to cast the damage source entity to given class type or returns null otherwise. */
+	@Nullable
+	public static < NewType > NewType castEntityIfPossible( Class< NewType > newClass, DamageSource damageSource ) {
+		return CommonHelper.castIfPossible( newClass, damageSource.getEntity() );
+	}
+
+	/** Tries to cast the damage source direct entity to given class type or returns null otherwise. */
+	@Nullable
+	public static < NewType > NewType castDirectEntityIfPossible( Class< NewType > newClass, DamageSource damageSource ) {
+		return CommonHelper.castIfPossible( newClass, damageSource.getDirectEntity() );
+	}
+
+	/** Checks whether entity and direct entity from given damage sources are instances of given classes. */
+	public static < Type1, Type2 > boolean areEntitiesInstancesOf( DamageSource damageSource, Class< Type1 > entityClass,
+		Class< Type2 > directEntityClass
+	) {
+		return entityClass.isInstance( damageSource.getEntity() ) && directEntityClass.isInstance( damageSource.getDirectEntity() );
+	}
+
 	/** Returns given entity class from damage source or null if casting was impossible. */
 	@Nullable
+	@Deprecated
 	public static < EntityClass extends Entity > EntityClass getEntityFromDamageSource( Class< EntityClass > entityClass, DamageSource source ) {
 		return CommonHelper.castIfPossible( entityClass, source.getEntity() );
 	}
@@ -21,6 +41,7 @@ public class DamageHelper {
 	 Returns null if casting was impossible.
 	 */
 	@Nullable
+	@Deprecated
 	public static Player getPlayerFromDamageSource( DamageSource damageSource ) {
 		return CommonHelper.castIfPossible( Player.class, damageSource.getEntity() );
 	}
@@ -29,7 +50,8 @@ public class DamageHelper {
 	 Checks whether damage source from directly from living entity.
 	 For example if skeleton shoots an arrow then it is not direct damage.
 	 */
+	@Deprecated
 	public static boolean isDirectDamageFromLivingEntity( DamageSource source ) {
-		return source.getEntity() instanceof LivingEntity && source.getDirectEntity() instanceof LivingEntity;
+		return areEntitiesInstancesOf( source, LivingEntity.class, LivingEntity.class );
 	}
 }
