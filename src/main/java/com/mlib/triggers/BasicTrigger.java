939a04627128ee557dfa477129e5d2fd03f37443
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-19 14:10:46 +0200

    Fixed invalid BasicTrigger structure

--- src/main/java/com/mlib/triggers/BasicTrigger.java
+++ src/main/java/com/mlib/triggers/BasicTrigger.java
@@ -2,7 +2,6 @@ package com.mlib.triggers;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.mlib.MajruszLibrary;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.advancements.critereon.*;
 import net.minecraft.resources.ResourceLocation;
@@ -10,11 +9,22 @@ import net.minecraft.server.level.ServerPlayer;
 
 /** Trigger that is called without any parameters, only with its id. */
 public class BasicTrigger extends SimpleCriterionTrigger< BasicTrigger.Instance > {
-	private static final ResourceLocation ID = MajruszLibrary.getLocation( "basic_trigger" );
-	private final String modID;
+	private final ResourceLocation triggerID;
 
 	public BasicTrigger( String modID ) {
-		this.modID = modID;
+		this.triggerID = new ResourceLocation( modID, "basic_trigger" );
+	}
+
+	@Override
+	public ResourceLocation getId() {
+		return this.triggerID;
+	}
+
+	@Override
+	public BasicTrigger.Instance createInstance( JsonObject json, EntityPredicate.Composite predicate, DeserializationContext conditions ) {
+		JsonElement triggerType = json.get( "type" );
+
+		return new BasicTrigger.Instance( this.triggerID, predicate, triggerType.getAsString() );
 	}
 
 	/** Creates and registers instance of BasicTrigger with given mod id. */
@@ -22,46 +32,31 @@ public class BasicTrigger extends SimpleCriterionTrigger< BasicTrigger.Instance 
 		return CriteriaTriggers.register( new BasicTrigger( modID ) );
 	}
 
-	@Override
-	public ResourceLocation getId() {
-		return ID;
-	}
-
-	@Override
-	public BasicTrigger.Instance createInstance( JsonObject json, EntityPredicate.Composite predicate, DeserializationContext conditions ) {
-		JsonElement modID = json.get( "mod_id" );
-		JsonElement triggerType = json.get( "type" );
-
-		return new BasicTrigger.Instance( predicate, modID.getAsString(), triggerType.getAsString() );
-	}
-
 	/** Triggers an advancement for given player. */
 	public void trigger( ServerPlayer player, String triggerType ) {
-		this.trigger( player, instance->instance.test( this.modID, triggerType ) );
+		this.trigger( player, instance->instance.test( triggerType ) );
 	}
 
 	protected static class Instance extends AbstractCriterionTriggerInstance {
-		private final String modID, triggerType;
+		private final String triggerType;
 
-		public Instance( EntityPredicate.Composite predicate, String modID, String triggerType ) {
-			super( BasicTrigger.ID, predicate );
+		public Instance( ResourceLocation triggerID, EntityPredicate.Composite predicate, String triggerType ) {
+			super( triggerID, predicate );
 
-			this.modID = modID;
 			this.triggerType = triggerType;
 		}
 
 		@Override
 		public JsonObject serializeToJson( SerializationContext conditions ) {
 			JsonObject jsonObject = super.serializeToJson( conditions );
-			jsonObject.addProperty( "mod_id", this.modID );
 			jsonObject.addProperty( "type", this.triggerType );
 
 			return jsonObject;
 		}
 
 		/** Checks whether conditions were met. */
-		public boolean test( String modID, String triggerType ) {
-			return this.modID.equals( modID ) && this.triggerType.equals( triggerType );
+		public boolean test( String triggerType ) {
+			return this.triggerType.equals( triggerType );
 		}
 	}
 }
