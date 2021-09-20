8c0acfb65c28fe6d0cff045b5d33ee8466ec5027
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-20 19:34:30 +0200

    Update BaseCommand.java

--- src/main/java/com/mlib/commands/BaseCommand.java
+++ src/main/java/com/mlib/commands/BaseCommand.java
@@ -1,6 +1,7 @@
 package com.mlib.commands;
 
-import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
@@ -13,33 +14,53 @@ import net.minecraft.commands.arguments.coordinates.Vec3Argument;
 import net.minecraft.commands.arguments.selector.EntitySelector;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.server.command.EnumArgument;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
 /** Base class for easier creating simple commands. */
-public abstract class CommandBase {
+public class BaseCommand {
 	private static final String DEFAULT_LOCATION_ARGUMENT = "location";
 	private static final String DEFAULT_ENTITY_ARGUMENT = "entity";
 	private static final String DEFAULT_ENTITIES_ARGUMENT = "entities";
-	private static final List< CommandBase > COMMANDS = new ArrayList<>();
-
-	public CommandBase() {
-		COMMANDS.add( this );
-	}
-
-	/** Registers all created commands. (should be called only be Majrusz Library!) */
-	public static void registerAll( RegisterCommandsEvent event ) {
-		for( CommandBase command : COMMANDS )
-			command.register( event.getDispatcher() );
-	}
 
 	public static Predicate< CommandSourceStack > hasPermission( int requiredLevel ) {
 		return commandSourceStack->commandSourceStack.hasPermission( requiredLevel );
+	}
+
+	/** Adds integer argument. */
+	public static RequiredArgumentBuilder< CommandSourceStack, Integer > integer( String name ) {
+		return Commands.argument( name, IntegerArgumentType.integer() );
+	}
+
+	/** Adds integer argument. */
+	public static RequiredArgumentBuilder< CommandSourceStack, Integer > integer( String name, int min ) {
+		return Commands.argument( name, IntegerArgumentType.integer( min ) );
+	}
+
+	/** Adds integer argument. */
+	public static RequiredArgumentBuilder< CommandSourceStack, Integer > integer( String name, int min, int max ) {
+		return Commands.argument( name, IntegerArgumentType.integer( min, max ) );
+	}
+
+	/** Returns integer argument. */
+	public static int getInteger( CommandContext< CommandSourceStack > context, String name ) {
+		return IntegerArgumentType.getInteger( context, name );
+	}
+
+	/** Adds enum argument. */
+	public static < EnumType extends Enum< EnumType > > RequiredArgumentBuilder< CommandSourceStack, EnumType > enumArgument( String name, Class< EnumType > enumClass ) {
+		return Commands.argument( name, EnumArgument.enumArgument( enumClass ) );
+	}
+
+	/** Returns enum argument. */
+	public static < EnumType extends Enum< EnumType > > EnumType getEnum( CommandContext< CommandSourceStack > context, String name, Class< EnumType > enumClass ) {
+		return context.getArgument( name, enumClass );
 	}
 
 	/** Adds text argument. */
@@ -117,6 +138,50 @@ public abstract class CommandBase {
 		return getEntities( context, DEFAULT_ENTITIES_ARGUMENT );
 	}
 
-	/** Registers this command. */
-	protected abstract void register( CommandDispatcher< CommandSourceStack > dispatcher );
+	/** Stores information about command. */
+	public static class Data {
+		public final List< ArgumentBuilder< CommandSourceStack, ? > > arguments = new ArrayList<>();
+		public final List< Predicate< CommandSourceStack > > requirements = new ArrayList<>();
+		public final LiteralArgumentBuilder< CommandSourceStack > command;
+
+		@SafeVarargs
+		public Data( LiteralArgumentBuilder< CommandSourceStack > command, ArgumentBuilder< CommandSourceStack, ? >... arguments ) {
+			this.command = command;
+			addArguments( arguments );
+		}
+
+		@SafeVarargs
+		public Data( Predicate< CommandSourceStack > requirement, LiteralArgumentBuilder< CommandSourceStack > command,
+			ArgumentBuilder< CommandSourceStack, ? >... arguments
+		) {
+			this( command, arguments );
+			addRequirements( requirement );
+		}
+
+		@SafeVarargs
+		public final void addArguments( ArgumentBuilder< CommandSourceStack, ? >... arguments ) {
+			Collections.addAll( this.arguments, arguments );
+		}
+
+		@SafeVarargs
+		public final void addRequirements( Predicate< CommandSourceStack >... requirements ) {
+			Collections.addAll( this.requirements, requirements );
+		}
+
+		public Data copy() {
+			Data dataCopy = new Data( this.command );
+			dataCopy.arguments.addAll( this.arguments );
+			dataCopy.requirements.addAll( this.requirements );
+
+			return dataCopy;
+		}
+
+		@SafeVarargs
+		public final Data extendCopy( ArgumentBuilder< CommandSourceStack, ? >... arguments ) {
+			Data dataCopy = copy();
+			dataCopy.addArguments( arguments );
+
+			return dataCopy;
+		}
+	}
 }
