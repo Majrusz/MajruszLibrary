e6d48d42eec94f985b9af1b97a9524790ee2897d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-28 18:18:09 +0200

    Update FrameVec3.java

--- src/main/java/com/mlib/animations/FrameVec3.java
+++ src/main/java/com/mlib/animations/FrameVec3.java
@@ -12,6 +12,14 @@ public class FrameVec3 extends Frame< Vec3 > {
 		super( startDuration, value );
 	}
 
+	public FrameVec3( float startDuration, double x, double y, double z, InterpolationType interpolationType ) {
+		this( startDuration, new Vec3( x, y, z ), interpolationType );
+	}
+
+	public FrameVec3( float startDuration, double x, double y, double z ) {
+		this( startDuration, new Vec3( x, y, z ) );
+	}
+
 	@Override
 	public Vec3 interpolate( float ratio, Frame< Vec3 > nextFrame ) {
 		double x = Mth.lerp( ratio, getValue().x, nextFrame.getValue().x );
