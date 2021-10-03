9c19ea005b2410ab6d7234cbe83868f5a63c32a7
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-10-03 10:18:43 +0200

    Update CommonHelper.java

--- src/main/java/com/mlib/CommonHelper.java
+++ src/main/java/com/mlib/CommonHelper.java
@@ -1,5 +1,7 @@
 package com.mlib;
 
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
 import javax.annotation.Nullable;
 
 /** Class with common functions for most of the Minecraft's classes. */
@@ -8,5 +10,11 @@ public class CommonHelper {
 	@Nullable
 	public static < NewType > NewType castIfPossible( Class< NewType > newClass, Object object ) {
 		return newClass.isInstance( object ) ? newClass.cast( object ) : null;
+	}
+
+	/** Returns registry name whether possible or null otherwise. */
+	@Nullable
+	public static < Type extends IForgeRegistryEntry< Type > > String getRegistryNameString( Type entry ) {
+		return entry.getRegistryName() != null ? entry.getRegistryName().toString() : null;
 	}
 }
