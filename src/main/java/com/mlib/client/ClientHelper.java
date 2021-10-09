661201277e6e80b57112bde3ff4d54469c326400
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-10-09 13:52:23 +0200

    Update ClientHelper.java

--- src/main/java/com/mlib/client/ClientHelper.java
+++ src/main/java/com/mlib/client/ClientHelper.java
@@ -1,10 +1,12 @@
 package com.mlib.client;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+@OnlyIn( Dist.CLIENT )
 @Mod.EventBusSubscriber( value = Dist.CLIENT )
 public class ClientHelper {
 	protected static int shiftLastAction = 0;
