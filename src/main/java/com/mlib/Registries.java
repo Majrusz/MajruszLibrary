bb9517c6b3123456dbc32304f521006e7fc968c8
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-21 18:10:49 +0200

    Used new "registry handler"

---
+++ src/main/java/com/mlib/Registries.java
@@ -0,0 +1,26 @@
+package com.mlib;
+
+import com.mlib.loot_modifiers.*;
+import com.mlib.registries.DeferredRegisterHelper;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class Registries {
+	static final DeferredRegisterHelper HELPER = new DeferredRegisterHelper( MajruszLibrary.MOD_ID );
+	static final DeferredRegister< GlobalLootModifierSerializer< ? > > LOOT_MODIFIERS = HELPER.create( ForgeRegistries.Keys.LOOT_MODIFIER_SERIALIZERS );
+	static {
+		LOOT_MODIFIERS.register( "any_situation", AnyModification.Serializer::new );
+		LOOT_MODIFIERS.register( "harvest_crop", HarvestCrop.Serializer::new );
+		LOOT_MODIFIERS.register( "smelting_items", SmeltingItems.Serializer::new );
+	}
+
+	public static void initialize() {
+		HELPER.registerAll();
+	}
+
+	public static ResourceLocation getLocation( String register ) {
+		return HELPER.getLocation( register );
+	}
+}
