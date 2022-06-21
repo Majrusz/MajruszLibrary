b02735e81c314351323540cccd9c6c50d9230dc9
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-21 18:08:19 +0200

    Code refactored

--- src/main/java/com/mlib/registries/DeferredRegisterHelper.java
+++ src/main/java/com/mlib/registries/DeferredRegisterHelper.java
@@ -2,33 +2,36 @@ package com.mlib.registries;
 
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class DeferredRegisterHelper {
 	private final String modId;
-	private final List< Consumer< IEventBus > > consumers = new ArrayList<>();
+	private final List< DeferredRegister< ? > > registers = new ArrayList<>();
 
 	public DeferredRegisterHelper( String modId ) {
 		this.modId = modId;
 	}
 
-	public < Type > void create( ResourceKey< Registry< Type > > key, Consumer< DeferredRegister< Type > > objectsToRegister ) {
+	public < Type > DeferredRegister< Type > create( ResourceKey< Registry< Type > > key ) {
 		DeferredRegister< Type > register = DeferredRegister.create( key, this.modId );
-		this.consumers.add( modEventBus -> {
-			objectsToRegister.accept( register );
-			register.register( modEventBus );
-		} );
+		this.registers.add( register );
+
+		return register;
 	}
 
 	public void registerAll() {
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		this.consumers.forEach( consumer -> consumer.accept( modEventBus ) );
+		this.registers.forEach( register -> register.register( modEventBus ) );
+	}
+
+	public ResourceLocation getLocation( String register ) {
+		return new ResourceLocation( this.modId, register );
 	}
 }
