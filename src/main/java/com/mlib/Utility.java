6f647c9f2e6ff38544510feea36545e93ab58537
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-23 20:41:49 +0200

    Update Utility.java

--- src/main/java/com/mlib/Utility.java
+++ src/main/java/com/mlib/Utility.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 
 import javax.annotation.Nullable;
+import java.util.TreeMap;
 
 public class Utility {
 	public static final int TICKS_IN_SECOND = 20;
@@ -47,5 +48,25 @@ public class Utility {
 
 	public static String getRegistryString( EntityType< ? > entityType ) {
 		return getRegistryKey( entityType ).toString();
+	}
+
+	public static String toRoman( int number ) {
+		int nearestKey = ROMAN_LETTERS.floorKey( number );
+
+		return number == nearestKey ? ROMAN_LETTERS.get( number ) : ROMAN_LETTERS.get( nearestKey ) + toRoman( number - nearestKey );
+	}
+
+	private static final TreeMap< Integer, String > ROMAN_LETTERS = new TreeMap<>();
+
+	static {
+		ROMAN_LETTERS.put( 100, "C" );
+		ROMAN_LETTERS.put( 90, "XC" );
+		ROMAN_LETTERS.put( 50, "L" );
+		ROMAN_LETTERS.put( 40, "XL" );
+		ROMAN_LETTERS.put( 10, "X" );
+		ROMAN_LETTERS.put( 9, "IX" );
+		ROMAN_LETTERS.put( 5, "V" );
+		ROMAN_LETTERS.put( 4, "IV" );
+		ROMAN_LETTERS.put( 1, "I" );
 	}
 }
