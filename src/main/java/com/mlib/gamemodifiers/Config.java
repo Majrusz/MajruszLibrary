1106c3ae279a2889e89a1e9791b5228ddba9974d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-24 18:52:50 +0200

    Reworked effect config

--- src/main/java/com/mlib/gamemodifiers/Config.java
+++ src/main/java/com/mlib/gamemodifiers/Config.java
@@ -3,6 +3,12 @@ package com.mlib.gamemodifiers;
 import com.mlib.config.ConfigGroup;
 import com.mlib.config.DoubleConfig;
 import com.mlib.config.IntegerConfig;
+import com.mlib.effects.EffectHelper;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.LivingEntity;
+
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public abstract class Config {
 	final String groupName;
@@ -22,13 +28,43 @@ public abstract class Config {
 	public static class Effect extends Config {
 		static final int MIN_AMPLIFIER = 0, MAX_AMPLIFIER = 10;
 		static final double MIN_DURATION = 1.0, MAX_DURATION = 99.0;
+		static final double MIN_LIMIT = 5.0, MAX_LIMIT = 999.0;
+		final Supplier< MobEffect > effect;
 		final IntegerConfig amplifier;
 		final DoubleConfig duration;
+		final Optional< DoubleConfig > maxDuration;
 
-		public Effect( String groupName, int amplifier, double duration ) {
+		public Effect( String groupName, Supplier< MobEffect > effect, int amplifier, double duration, Optional< Double > maxDuration ) {
 			super( groupName, "" );
+			this.effect = effect;
 			this.amplifier = new IntegerConfig( "amplifier", "Level of the effect to apply.", false, amplifier, MIN_AMPLIFIER, MAX_AMPLIFIER );
 			this.duration = new DoubleConfig( "duration", "Duration in seconds.", false, duration, MIN_DURATION, MAX_DURATION );
+			this.maxDuration = maxDuration.map( value->new DoubleConfig( "maximum_duration", "Maximum duration in seconds it can reach.", false, value, MIN_LIMIT, MAX_LIMIT ) );
+		}
+
+		public Effect( String groupName, Supplier< MobEffect > effect, int amplifier, double duration, double maxDuration ) {
+			this( groupName, effect, amplifier, duration, Optional.of( maxDuration ) );
+		}
+
+		public Effect( String groupName, Supplier< MobEffect > effect, int amplifier, double duration ) {
+			this( groupName, effect, amplifier, duration, Optional.empty() );
+		}
+
+		@Override
+		public void setup( ConfigGroup group ) {
+			group.addConfigs( this.amplifier, this.duration );
+		}
+
+		public void apply( LivingEntity entity ) {
+			if( this.maxDuration.isPresent() ) {
+				EffectHelper.stackEffectIfPossible( entity, getEffect(), getDuration(), getAmplifier(), getMaxDuration() );
+			} else {
+				EffectHelper.applyEffectIfPossible( entity, getEffect(), getDuration(), getAmplifier() );
+			}
+		}
+
+		public MobEffect getEffect() {
+			return this.effect.get();
 		}
 
 		public int getAmplifier() {
@@ -39,9 +75,10 @@ public abstract class Config {
 			return this.duration.asTicks();
 		}
 
-		@Override
-		public void setup( ConfigGroup group ) {
-			group.addConfigs( this.amplifier, this.duration );
+		public int getMaxDuration() {
+			assert this.maxDuration.isPresent();
+
+			return this.maxDuration.get().asTicks();
 		}
 	}
 }
