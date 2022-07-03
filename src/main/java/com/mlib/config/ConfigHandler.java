3c56b08ebd9e31fb822b8bef2fc6a7c658328366
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-07-03 17:56:25 +0200

    Update ConfigHandler.java

--- src/main/java/com/mlib/config/ConfigHandler.java
+++ src/main/java/com/mlib/config/ConfigHandler.java
@@ -13,7 +13,7 @@ import java.util.List;
 /** Handler that makes creating new configs much easier. */
 public class ConfigHandler {
 	public ForgeConfigSpec configSpec;
-	protected List< ConfigGroup > configGroups = new ArrayList<>();
+	protected List< UserConfig > configs = new ArrayList<>();
 	private final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 	private final ModConfig.Type type;
 	private final String filename;
@@ -25,46 +25,56 @@ public class ConfigHandler {
 
 	/** Registers all configs (all config values are valid after this call). */
 	public void register( final ModLoadingContext modLoadingContext ) {
-		for( ConfigGroup configGroup : this.configGroups )
-			configGroup.build( this.builder );
+		for( UserConfig config : this.configs )
+			config.build( this.builder );
 
 		this.configSpec = this.builder.build();
 		modLoadingContext.registerConfig( this.type, this.configSpec, this.filename );
 		MinecraftForge.EVENT_BUS.post( new ConfigsLoadedEvent( this ) );
 	}
 
+	public < ConfigType extends UserConfig > ConfigType addConfig( ConfigType config ) {
+		this.configs.add( config );
+
+		return config;
+	}
+
+	public void addConfigs( UserConfig... configs ) {
+		this.configs.addAll( List.of( configs ) );
+	}
+
 	public < ConfigGroupType extends ConfigGroup > ConfigGroupType addGroup( ConfigGroupType configGroup ) {
-		this.configGroups.add( configGroup );
+		this.configs.add( configGroup );
 
 		return configGroup;
 	}
 
 	public ConfigGroup addNewGroup( String groupName, String comment, UserConfig... configs ) {
 		ConfigGroup configGroup = new ConfigGroup( groupName, comment, configs );
-		this.configGroups.add( configGroup );
+		this.configs.add( configGroup );
 
 		return configGroup;
 	}
 
 	public ConfigGroup addNewGameModifierGroup( String configKey ) {
 		ConfigGroup configGroup = GameModifier.addNewGroup( configKey );
-		this.configGroups.add( configGroup );
+		this.configs.add( configGroup );
 
 		return configGroup;
 	}
 
 	public ConfigGroup addNewGameModifierGroup( String configKey, String groupName, String comment ) {
 		ConfigGroup configGroup = GameModifier.addNewGroup( configKey, groupName, comment );
-		this.configGroups.add( configGroup );
+		this.configs.add( configGroup );
 
 		return configGroup;
 	}
 
-	public ModConfig.Type GetType() {
+	public ModConfig.Type getType() {
 		return this.type;
 	}
 
-	public String GetFilename() {
+	public String getFilename() {
 		return this.filename;
 	}
 }
