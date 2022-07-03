c25f10175128dc548a7a7609a08f1afa74ef4acc
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-07-03 15:11:43 +0200

    Update EquipmentSlots.java

--- src/main/java/com/mlib/EquipmentSlots.java
+++ src/main/java/com/mlib/EquipmentSlots.java
@@ -12,4 +12,11 @@ public class EquipmentSlots {
 	public static EquipmentSlot[] ARMOR_AND_HANDS = new EquipmentSlot[]{
 		EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND
 	};
+	// below are default values wrapped into arrays
+	public static EquipmentSlot[] MAINHAND = new EquipmentSlot[]{ EquipmentSlot.MAINHAND };
+	public static EquipmentSlot[] OFFHAND = new EquipmentSlot[]{ EquipmentSlot.OFFHAND };
+	public static EquipmentSlot[] HEAD = new EquipmentSlot[]{ EquipmentSlot.HEAD };
+	public static EquipmentSlot[] CHEST = new EquipmentSlot[]{ EquipmentSlot.CHEST };
+	public static EquipmentSlot[] LEGS = new EquipmentSlot[]{ EquipmentSlot.LEGS };
+	public static EquipmentSlot[] FEET = new EquipmentSlot[]{ EquipmentSlot.FEET };
 }
