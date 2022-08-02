4c4aeaf259d293fa9e99889d90c9a64e88b2c6a4
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-02 18:28:41 +0200

    Update OnLootLevelData.java

--- src/main/java/com/mlib/gamemodifiers/data/OnLootLevelData.java
+++ src/main/java/com/mlib/gamemodifiers/data/OnLootLevelData.java
@@ -6,8 +6,10 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LootingLevelEvent;
 
+import javax.annotation.Nullable;
+
 public class OnLootLevelData extends ContextData.Event< LootingLevelEvent > {
-	public final DamageSource source;
+	@Nullable public final DamageSource source;
 
 	public OnLootLevelData( LootingLevelEvent event ) {
 		super( event.getDamageSource() != null ? Utility.castIfPossible( LivingEntity.class, event.getDamageSource().getEntity() ) : null, event );
