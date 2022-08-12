ac7ec8f650f4c1fff425d4ce9b7497473bb031c7
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-12 19:45:22 +0200

    Update GameModifiersHolder.java

--- src/main/java/com/mlib/gamemodifiers/GameModifiersHolder.java
+++ src/main/java/com/mlib/gamemodifiers/GameModifiersHolder.java
@@ -1,19 +1,17 @@
 package com.mlib.gamemodifiers;
 
-import com.mlib.config.ConfigGroup;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 public class GameModifiersHolder< RegistryType > {
 	final List< GameModifier > modifiers = new ArrayList<>();
-	final ConfigGroup group;
+	final String configKey;
 	Supplier< RegistryType > lazySupplier;
 
-	public GameModifiersHolder( ConfigGroup group, Supplier< RegistryType > lazySupplier ) {
-		this.group = group;
+	public GameModifiersHolder( String configKey, Supplier< RegistryType > lazySupplier ) {
+		this.configKey = configKey;
 		this.lazySupplier = ()->{
 			RegistryType register = lazySupplier.get();
 			this.lazySupplier = ()->register;
@@ -25,7 +23,7 @@ public class GameModifiersHolder< RegistryType > {
 		return this.lazySupplier.get();
 	}
 
-	public void addModifier( Function< Supplier< RegistryType >, GameModifier > provider ) {
-		this.modifiers.add( provider.apply( this::getRegistry ) );
+	public void addModifier( BiFunction< Supplier< RegistryType >, String, GameModifier > provider ) {
+		this.modifiers.add( provider.apply( this::getRegistry, this.configKey ) );
 	}
 }
