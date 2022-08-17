8784171e75bdf15464cfd2a39864d2e10bb7be64
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-17 20:29:40 +0200

    Update OnItemFishedData.java

--- src/main/java/com/mlib/gamemodifiers/data/OnItemFishedData.java
+++ src/main/java/com/mlib/gamemodifiers/data/OnItemFishedData.java
@@ -3,16 +3,19 @@ package com.mlib.gamemodifiers.data;
 import com.mlib.gamemodifiers.ContextData;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.player.ItemFishedEvent;
 
 public class OnItemFishedData extends ContextData.Event< ItemFishedEvent > {
 	public final Player player;
+	public final FishingHook hook;
 	public final NonNullList< ItemStack > drops;
 
 	public OnItemFishedData( ItemFishedEvent event ) {
 		super( event.getEntity(), event );
 		this.player = event.getEntity();
+		this.hook = event.getHookEntity();
 		this.drops = event.getDrops();
 	}
 }
