08be0874840d28c5597a3fb83724f9703fde1ec7
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-27 22:13:31 +0200

    Reworked OnSpawned

--- src/main/java/com/mlib/gamemodifiers/contexts/OnSpawnedContext.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnSpawnedContext.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
+@Deprecated
 @Mod.EventBusSubscriber
 public class OnSpawnedContext extends ContextBase< OnSpawnedData > {
 	static final List< OnSpawnedContext > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
