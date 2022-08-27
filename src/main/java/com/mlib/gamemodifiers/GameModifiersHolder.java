65478730080a3df9910b5811ed72f0c3b89b0620
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-27 12:10:03 +0200

    Update GameModifiersHolder.java

--- src/main/java/com/mlib/gamemodifiers/GameModifiersHolder.java
+++ src/main/java/com/mlib/gamemodifiers/GameModifiersHolder.java
@@ -3,6 +3,7 @@ package com.mlib.gamemodifiers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class GameModifiersHolder< RegistryType extends IRegistrable > {
@@ -26,6 +27,14 @@ public class GameModifiersHolder< RegistryType extends IRegistrable > {
 
 	public List< GameModifier > getModifiers() {
 		return this.modifiers;
+	}
+
+	public < Type > void forEach( Class< Type > classType, Consumer< Type > consumer ) {
+		for( GameModifier modifier : this.modifiers ) {
+			if( classType.isInstance( modifier ) ) {
+				consumer.accept( classType.cast( modifier ) );
+			}
+		}
 	}
 
 	public void addModifier( BiFunction< Supplier< RegistryType >, String, GameModifier > provider ) {
