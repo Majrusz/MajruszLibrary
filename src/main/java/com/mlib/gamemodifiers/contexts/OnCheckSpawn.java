8902ca46c64f48c5bf8d39d8d4cf70435beb86d5
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 09:07:39 +0100

    Reworked OnCheckSpawn context

--- src/main/java/com/mlib/gamemodifiers/contexts/OnCheckSpawn.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnCheckSpawn.java
@@ -2,24 +2,22 @@ package com.mlib.gamemodifiers.contexts;
 
 import com.mlib.gamemodifiers.ContextBase;
 import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
 import com.mlib.gamemodifiers.parameters.ContextParameters;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class OnCheckSpawn {
 	@Mod.EventBusSubscriber
 	public static class Context extends ContextBase< Data > {
-		static final List< Context > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
 
 		public Context( Consumer< Data > consumer, ContextParameters params ) {
 			super( Data.class, consumer, params );
-			ContextBase.addSorted( CONTEXTS, this );
+			CONTEXTS.add( this );
 		}
 
 		public Context( Consumer< Data > consumer ) {
@@ -28,7 +26,7 @@ public class OnCheckSpawn {
 
 		@SubscribeEvent
 		public static void onSpawnCheck( LivingSpawnEvent.CheckSpawn event ) {
-			ContextBase.accept( CONTEXTS, new Data( event ) );
+			CONTEXTS.accept( new Data( event ) );
 		}
 	}
 
