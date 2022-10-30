f0c400ea8bc8756ce23ddfe54bff148ebc327374
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 09:20:52 +0100

    Reworked OnItemSwingDuration context

--- src/main/java/com/mlib/gamemodifiers/contexts/OnItemSwingDurationContext.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnItemSwingDurationContext.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
+@Deprecated
 @Mod.EventBusSubscriber
 public class OnItemSwingDurationContext extends ContextBase< OnItemSwingDurationData > {
 	static final List< OnItemSwingDurationContext > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
--- src/main/java/com/mlib/gamemodifiers/data/OnItemSwingDurationData.java
+++ src/main/java/com/mlib/gamemodifiers/data/OnItemSwingDurationData.java
@@ -3,6 +3,7 @@ package com.mlib.gamemodifiers.data;
 import com.mlib.events.ItemSwingDurationEvent;
 import com.mlib.gamemodifiers.ContextData;
 
+@Deprecated
 public class OnItemSwingDurationData extends ContextData.Event< ItemSwingDurationEvent > {
 	public OnItemSwingDurationData( ItemSwingDurationEvent event ) {
 		super( event.entity, event );
