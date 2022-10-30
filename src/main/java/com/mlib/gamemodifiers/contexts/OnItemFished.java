5bb7429e12fc25f3146a9059439cbe76b98ce37a
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 09:17:56 +0100

    Reworked OnItemFished context

--- src/main/java/com/mlib/gamemodifiers/contexts/OnItemFished.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnItemFished.java
@@ -2,6 +2,7 @@ package com.mlib.gamemodifiers.contexts;
 
 import com.mlib.gamemodifiers.ContextBase;
 import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
 import com.mlib.gamemodifiers.parameters.ContextParameters;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Player;
@@ -11,19 +12,16 @@ import net.minecraftforge.event.entity.player.ItemFishedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class OnItemFished {
 	@Mod.EventBusSubscriber
 	public static class Context extends ContextBase< Data > {
-		static final List< Context > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
 
 		public Context( Consumer< Data > consumer, ContextParameters params ) {
 			super( Data.class, consumer, params );
-			ContextBase.addSorted( CONTEXTS, this );
+			CONTEXTS.add( this );
 		}
 
 		public Context( Consumer< Data > consumer ) {
@@ -32,7 +30,7 @@ public class OnItemFished {
 
 		@SubscribeEvent
 		public static void onDimensionChanged( ItemFishedEvent event ) {
-			ContextBase.accept( CONTEXTS, new Data( event ) );
+			CONTEXTS.accept( new Data( event ) );
 		}
 	}
 
