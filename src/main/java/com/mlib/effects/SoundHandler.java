41ff084f74861084c071b77ca086ebf093f63826
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 11:16:33 +0100

    Added more common sounds into handler

--- src/main/java/com/mlib/effects/SoundHandler.java
+++ src/main/java/com/mlib/effects/SoundHandler.java
@@ -10,16 +10,24 @@ import net.minecraft.world.phys.Vec3;
 import java.util.function.Supplier;
 
 public class SoundHandler {
-	public static final SoundHandler AWARD = new SoundHandler( SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.AMBIENT, ()->0.05f );
-	public static final SoundHandler BONE_MEAL = new SoundHandler( SoundEvents.BONE_MEAL_USE, SoundSource.AMBIENT, ()->0.6f );
-	public static final SoundHandler FIRE_EXTINGUISH = new SoundHandler( SoundEvents.GENERIC_EXTINGUISH_FIRE, SoundSource.AMBIENT, ()->1.0f );
-	public static final SoundHandler PICKUP = new SoundHandler( SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, ()->0.05f );
-	public static final SoundHandler SMELT = new SoundHandler( SoundEvents.FIRECHARGE_USE, SoundSource.AMBIENT, ()->0.05f );
+	public static final float DEFAULT_VOLUME = 0.7f;
+	public static final SoundHandler AWARD = new SoundHandler( SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.AMBIENT, DEFAULT_VOLUME );
+	public static final SoundHandler DRINK = new SoundHandler( SoundEvents.GENERIC_DRINK, SoundSource.PLAYERS, DEFAULT_VOLUME );
+	public static final SoundHandler BONE_MEAL = new SoundHandler( SoundEvents.BONE_MEAL_USE, SoundSource.AMBIENT, DEFAULT_VOLUME );
+	public static final SoundHandler FIRE_EXTINGUISH = new SoundHandler( SoundEvents.GENERIC_EXTINGUISH_FIRE, SoundSource.AMBIENT, DEFAULT_VOLUME );
+	public static final SoundHandler ITEM_BREAK = new SoundHandler( SoundEvents.ITEM_BREAK, SoundSource.PLAYERS, DEFAULT_VOLUME );
+	public static final SoundHandler ITEM_PICKUP = new SoundHandler( SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, DEFAULT_VOLUME );
+	@Deprecated public static final SoundHandler PICKUP = new SoundHandler( SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, DEFAULT_VOLUME );
+	public static final SoundHandler SMELT = new SoundHandler( SoundEvents.FIRECHARGE_USE, SoundSource.AMBIENT, DEFAULT_VOLUME );
 
 	final SoundEvent event;
 	final SoundSource source;
 	final Supplier< Float > volumeProvider;
 	final Supplier< Float > pitchProvider;
+
+	public static Supplier< Float > randomized( float value ) {
+		return ()->value * Random.nextFloat( 0.8f, 1.2f );
+	}
 
 	public SoundHandler( SoundEvent event, SoundSource source, Supplier< Float > volumeProvider, Supplier< Float > pitchProvider ) {
 		this.event = event;
@@ -29,14 +37,22 @@ public class SoundHandler {
 	}
 
 	public SoundHandler( SoundEvent event, SoundSource source, Supplier< Float > volumeProvider ) {
-		this( event, source, volumeProvider, ()->Random.nextFloat( 0.8f, 1.2f ) );
+		this( event, source, volumeProvider, randomized( 1.0f ) );
 	}
 
 	public SoundHandler( SoundEvent event, SoundSource source, float volume ) {
-		this( event, source, ()->volume * Random.nextFloat( 0.8f, 1.2f ), ()->Random.nextFloat( 0.8f, 1.2f ) );
+		this( event, source, randomized( volume ), randomized( 1.0f ) );
 	}
 
 	public void play( ServerLevel level, Vec3 position ) {
-		level.playSound( null, position.x, position.y, position.z, this.event, this.source, this.volumeProvider.get(), this.pitchProvider.get() );
+		this.play( level, position, this.volumeProvider, this.pitchProvider );
+	}
+
+	public void play( ServerLevel level, Vec3 position, Supplier< Float > volumeProvider ) {
+		this.play( level, position, volumeProvider, this.pitchProvider );
+	}
+
+	public void play( ServerLevel level, Vec3 position, Supplier< Float > volumeProvider, Supplier< Float > pitchProvider ) {
+		level.playSound( null, position.x, position.y, position.z, this.event, this.source, volumeProvider.get(), pitchProvider.get() );
 	}
 }
