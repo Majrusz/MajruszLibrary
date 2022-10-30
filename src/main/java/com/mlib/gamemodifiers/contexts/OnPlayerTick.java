e14f11cc4ee43a2804845b090a67175c9405ffe8
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 10:23:55 +0100

    Reworked OnPlayerTick context

--- src/main/java/com/mlib/gamemodifiers/contexts/OnPlayerTick.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnPlayerTick.java
@@ -2,25 +2,23 @@ package com.mlib.gamemodifiers.contexts;
 
 import com.mlib.gamemodifiers.ContextBase;
 import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
 import com.mlib.gamemodifiers.parameters.ContextParameters;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class OnPlayerTick {
 	@Mod.EventBusSubscriber
 	public static class Context extends ContextBase< Data > {
-		static final List< Context > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
 
 		public Context( Consumer< Data > consumer, ContextParameters params ) {
 			super( Data.class, consumer, params );
-			ContextBase.addSorted( CONTEXTS, this );
+			CONTEXTS.add( this );
 		}
 
 		public Context( Consumer< Data > consumer ) {
@@ -29,7 +27,7 @@ public class OnPlayerTick {
 
 		@SubscribeEvent
 		public static void onPlayerTick( TickEvent.PlayerTickEvent event ) {
-			ContextBase.accept( CONTEXTS, new Data( event ) );
+			CONTEXTS.accept( new Data( event ) );
 		}
 	}
 
