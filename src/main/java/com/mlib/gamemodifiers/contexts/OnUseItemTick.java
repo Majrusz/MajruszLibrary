20461ed83e34ce124d0049e77c4205bc10ac05fd
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 10:41:53 +0100

    Reworked OnUseTimeTick context

--- src/main/java/com/mlib/gamemodifiers/contexts/OnUseItemTick.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnUseItemTick.java
@@ -2,25 +2,23 @@ package com.mlib.gamemodifiers.contexts;
 
 import com.mlib.gamemodifiers.ContextBase;
 import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
 import com.mlib.gamemodifiers.parameters.ContextParameters;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.Consumer;
 
 public class OnUseItemTick {
 	@Mod.EventBusSubscriber
 	public static class Context extends ContextBase< Data > {
-		static final List< Context > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
 
 		public Context( Consumer< Data > consumer, ContextParameters params ) {
 			super( Data.class, consumer, params );
-			ContextBase.addSorted( CONTEXTS, this );
+			CONTEXTS.add( this );
 		}
 
 		public Context( Consumer< Data > consumer ) {
@@ -29,7 +27,7 @@ public class OnUseItemTick {
 
 		@SubscribeEvent
 		public static void onTick( LivingEntityUseItemEvent.Tick event ) {
-			ContextBase.accept( CONTEXTS, new Data( event ) );
+			CONTEXTS.accept( new Data( event ) );
 		}
 	}
 
