8cbc808886f7e60825b932c2b4c91f8301e2f276
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-10-30 09:14:34 +0100

    Reworked OnEquipmentChanged context

---
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnEquipmentChanged.java
@@ -0,0 +1,38 @@
+package com.mlib.gamemodifiers.contexts;
+
+import com.mlib.gamemodifiers.ContextBase;
+import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
+import com.mlib.gamemodifiers.parameters.ContextParameters;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.function.Consumer;
+
+public class OnEquipmentChanged {
+	@Mod.EventBusSubscriber
+	public static class Context extends ContextBase< Data > {
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public Context( Consumer< Data > consumer, ContextParameters params ) {
+			super( Data.class, consumer, params );
+			CONTEXTS.add( this );
+		}
+
+		public Context( Consumer< Data > consumer ) {
+			this( consumer, new ContextParameters() );
+		}
+
+		@SubscribeEvent
+		public static void onEquipmentChanged( LivingEquipmentChangeEvent event ) {
+			CONTEXTS.accept( new Data( event ) );
+		}
+	}
+
+	public static class Data extends ContextData.Event< LivingEquipmentChangeEvent > {
+		public Data( LivingEquipmentChangeEvent event ) {
+			super( event.getEntity(), event );
+		}
+	}
+}
--- src/main/java/com/mlib/gamemodifiers/contexts/OnEquipmentChangedContext.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnEquipmentChangedContext.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
+@Deprecated
 @Mod.EventBusSubscriber
 public class OnEquipmentChangedContext extends ContextBase< OnEquipmentChangedData > {
 	static final List< OnEquipmentChangedContext > CONTEXTS = Collections.synchronizedList( new ArrayList<>() );
