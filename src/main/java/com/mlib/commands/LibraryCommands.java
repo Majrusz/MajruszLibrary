2bb58d45f5c21b404a72a69081ad55a4d1bf475f
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-11-08 21:09:46 +0100

    Added method called with different arguments

--- src/main/java/com/mlib/commands/LibraryCommands.java
+++ src/main/java/com/mlib/commands/LibraryCommands.java
@@ -4,6 +4,7 @@ import com.mlib.MajruszLibrary;
 import com.mlib.annotations.AutoInstance;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.world.phys.Vec3;
 
 public class LibraryCommands {
 	@AutoInstance
@@ -29,7 +30,19 @@ public class LibraryCommands {
 				.entity()
 				.execute( this::withEntity );
 
-			this.addBuilders( builder1, builder2 );
+			CommandBuilder builder3 = new CommandBuilder();
+			builder3.literal( "mlib" ).hasPermission( 4 ).literal( "mods3" );
+
+			CommandBuilder builder3_1 = builder3.copy();
+			builder3_1.execute( context->this.withRandomPosition( context, context.getSource().getPosition() ) );
+
+			CommandBuilder builder3_2 = builder3.copy();
+			builder3_2.position().execute( context->this.withRandomPosition( context, this.getPosition( context ) ) );
+
+			CommandBuilder builder3_3 = builder3.copy();
+			builder3_3.entity().execute( context->this.withRandomPosition( context, this.getEntity( context ).position() ) );
+
+			this.addBuilders( builder1, builder2, builder3_1, builder3_2, builder3_3 );
 		}
 
 		private int empty( CommandContext< CommandSourceStack > context ) {
@@ -58,6 +71,12 @@ public class LibraryCommands {
 
 		private int withEntity( CommandContext< CommandSourceStack > context ) {
 			MajruszLibrary.log( "test %s %s", this.getPosition( context ).toString(), this.getEntity( context ).toString() );
+
+			return 1;
+		}
+
+		private int withRandomPosition( CommandContext< CommandSourceStack > context, Vec3 position ) {
+			MajruszLibrary.log( "test %s", position.toString() );
 
 			return 1;
 		}
