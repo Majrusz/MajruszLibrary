cbb80e2ca911285573faf86bf7a54f23ca9b0d53
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-11-19 19:19:30 +0100

    Added OnItemEquipped context

---
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnItemEquipped.java
@@ -0,0 +1,47 @@
+package com.mlib.gamemodifiers.contexts;
+
+import com.mlib.gamemodifiers.ContextBase;
+import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
+import com.mlib.gamemodifiers.parameters.ContextParameters;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.function.Consumer;
+
+public class OnItemEquipped {
+	@Mod.EventBusSubscriber
+	public static class Context extends ContextBase< Data > {
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public Context( Consumer< Data > consumer, ContextParameters params ) {
+			super( Data.class, consumer, params );
+			CONTEXTS.add( this );
+		}
+
+		public Context( Consumer< Data > consumer ) {
+			this( consumer, new ContextParameters() );
+		}
+
+		@SubscribeEvent
+		public static void onItemEquipped( LivingEquipmentChangeEvent event ) {
+			CONTEXTS.accept( new Data( event ) );
+		}
+	}
+
+	public static class Data extends ContextData.Event< LivingEquipmentChangeEvent > {
+		public final EquipmentSlot slot;
+		public final ItemStack from;
+		public final ItemStack to;
+
+		public Data( LivingEquipmentChangeEvent event ) {
+			super( event.getEntity(), event );
+			this.slot = event.getSlot();
+			this.from = event.getFrom();
+			this.to = event.getTo();
+		}
+	}
+}
