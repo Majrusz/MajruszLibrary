b358099569a78ff5f3aca915f227633ff46c56b9
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-11-22 14:57:30 +0100

    Added OnEnderManAnger context

---
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnEnderManAnger.java
@@ -0,0 +1,45 @@
+package com.mlib.gamemodifiers.contexts;
+
+import com.mlib.gamemodifiers.ContextBase;
+import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
+import com.mlib.gamemodifiers.parameters.ContextParameters;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.EnderManAngerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.function.Consumer;
+
+public class OnEnderManAnger {
+	@Mod.EventBusSubscriber
+	public static class Context extends ContextBase< Data > {
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public Context( Consumer< Data > consumer, ContextParameters params ) {
+			super( Data.class, consumer, params );
+			CONTEXTS.add( this );
+		}
+
+		public Context( Consumer< Data > consumer ) {
+			this( consumer, new ContextParameters() );
+		}
+
+		@SubscribeEvent
+		public static void onAnger( EnderManAngerEvent event ) {
+			CONTEXTS.accept( new Data( event ) );
+		}
+	}
+
+	public static class Data extends ContextData.Event< EnderManAngerEvent > {
+		public final EnderMan enderMan;
+		public final Player player;
+
+		public Data( EnderManAngerEvent event ) {
+			super( event.getEntity(), event );
+			this.enderMan = event.getEntity();
+			this.player = event.getPlayer();
+		}
+	}
+}
