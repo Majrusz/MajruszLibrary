c2a4cf2942bb7d46ea4fbf49b7771ca2ed39b095
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-11-22 15:07:50 +0100

    Added Deprecated tag to all functions

--- src/main/java/com/mlib/effects/EffectHelper.java
+++ src/main/java/com/mlib/effects/EffectHelper.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.LivingEntity;
 /** Methods for easier handling effects. */
 @Deprecated( since = "Since 2.17.0 use MobEffectHelper" )
 public class EffectHelper {
+	@Deprecated( since = "Since 2.17.0 use MobEffectHelper" )
 	public static boolean applyEffectIfPossible( LivingEntity entity, MobEffectInstance effectInstance ) {
 		if( entity.canBeAffected( effectInstance ) ) {
 			entity.addEffect( effectInstance );
@@ -16,10 +17,12 @@ public class EffectHelper {
 		return false;
 	}
 
+	@Deprecated( since = "Since 2.17.0 use MobEffectHelper" )
 	public static boolean applyEffectIfPossible( LivingEntity entity, MobEffect effect, int ticks, int amplifier ) {
 		return applyEffectIfPossible( entity, new MobEffectInstance( effect, ticks, amplifier ) );
 	}
 
+	@Deprecated( since = "Since 2.17.0 use MobEffectHelper" )
 	public static boolean stackEffectIfPossible( LivingEntity entity, MobEffect effect, int ticks, int amplifier, int maxTicks ) {
 		MobEffectInstance previousEffectInstance = entity.getEffect( effect );
 		if( previousEffectInstance == null || previousEffectInstance.getAmplifier() > amplifier )
@@ -29,10 +32,12 @@ public class EffectHelper {
 		return applyEffectIfPossible( entity, effect, ticks, amplifier );
 	}
 
+	@Deprecated( since = "Since 2.17.0 use MobEffectHelper" )
 	public static boolean stackEffectIfPossible( LivingEntity entity, MobEffectInstance effectInstance, int maxTicks ) {
 		return stackEffectIfPossible( entity, effectInstance.getEffect(), effectInstance.getDuration(), effectInstance.getAmplifier(), maxTicks );
 	}
 
+	@Deprecated( since = "Since 2.17.0 use MobEffectHelper" )
 	/** Returns effect amplifier or -1 if entity does not have this effect active. */
 	public static int getEffectAmplifier( LivingEntity entity, MobEffect effect ) {
 		MobEffectInstance effectInstance = entity.getEffect( effect );
