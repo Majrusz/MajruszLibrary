8950466595fa49463c6c2de4eec00d049b90f270
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-11-23 21:14:08 +0100

    Added OnChorusFruitTeleport context

---
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnChorusFruitTeleport.java
@@ -0,0 +1,38 @@
+package com.mlib.gamemodifiers.contexts;
+
+import com.mlib.gamemodifiers.ContextBase;
+import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
+import com.mlib.gamemodifiers.parameters.ContextParameters;
+import net.minecraftforge.event.entity.EntityTeleportEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.function.Consumer;
+
+public class OnChorusFruitTeleport {
+	@Mod.EventBusSubscriber
+	public static class Context extends ContextBase< Data > {
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public Context( Consumer< Data > consumer, ContextParameters params ) {
+			super( Data.class, consumer, params );
+			CONTEXTS.add( this );
+		}
+
+		public Context( Consumer< Data > consumer ) {
+			this( consumer, new ContextParameters() );
+		}
+
+		@SubscribeEvent
+		public static void onTeleport( EntityTeleportEvent.ChorusFruit event ) {
+			CONTEXTS.accept( new Data( event ) );
+		}
+	}
+
+	public static class Data extends ContextData.Event< EntityTeleportEvent.ChorusFruit > {
+		public Data( EntityTeleportEvent.ChorusFruit event ) {
+			super( event.getEntityLiving(), event );
+		}
+	}
+}
