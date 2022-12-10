1d3a01983a8c4172882381c70ecc608898f9ffe0
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-10 20:59:37 +0100

    Added unit tests context

---
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnTestsRegister.java
@@ -0,0 +1,38 @@
+package com.mlib.gamemodifiers.contexts;
+
+import com.mlib.gamemodifiers.ContextBase;
+import com.mlib.gamemodifiers.ContextData;
+import com.mlib.gamemodifiers.Contexts;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.RegisterGameTestsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.function.Consumer;
+
+public class OnTestsRegister {
+	@Mod.EventBusSubscriber
+	public static class Context extends ContextBase< Data > {
+		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public Context( Consumer< Data > consumer, String name, String comment ) {
+			super( consumer, name, comment );
+			CONTEXTS.add( this );
+		}
+
+		public Context( Consumer< Data > consumer ) {
+			this( consumer, "", "" );
+		}
+
+		@SubscribeEvent
+		public static void registerTests( RegisterGameTestsEvent event ) {
+			CONTEXTS.accept( new Data( event ) );
+		}
+	}
+
+	public static class Data extends ContextData.Event< RegisterGameTestsEvent > {
+		public Data( RegisterGameTestsEvent event ) {
+			super( ( Entity )null, event );
+		}
+	}
+}
