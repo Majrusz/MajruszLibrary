5e9ba85e00cb538d10fddca8752524bab97e3e1d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-10 14:14:35 +0100

    Sort is delayed to the first call

--- src/main/java/com/mlib/gamemodifiers/Contexts.java
+++ src/main/java/com/mlib/gamemodifiers/Contexts.java
@@ -8,13 +8,18 @@ import java.util.List;
 
 public class Contexts< DataType extends ContextData, ContextType extends ContextBase< DataType > > {
 	final List< ContextType > contexts = Collections.synchronizedList( new ArrayList<>() );
+	boolean isSorted = false;
 
 	public void add( ContextType context ) {
 		this.contexts.add( context );
-		this.contexts.sort( Parameters.COMPARATOR );
 	}
 
 	public void accept( DataType data ) {
+		if( !this.isSorted ) {
+			this.contexts.sort( Parameters.COMPARATOR );
+			this.isSorted = true;
+		}
+
 		this.contexts.forEach( context->{
 			if( context.check( data ) ) {
 				context.consumer.accept( data );
