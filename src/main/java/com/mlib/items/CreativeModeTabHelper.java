0434ddcf1e4ce27168faed19c361c45e70b2c746
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-15 12:28:32 +0100

    Added CreativeModeTabHelper

---
+++ src/main/java/com/mlib/items/CreativeModeTabHelper.java
@@ -0,0 +1,39 @@
+package com.mlib.items;
+
+import com.mlib.Utility;
+import com.mlib.time.TimeHelper;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class CreativeModeTabHelper {
+	public static Consumer< Consumer< CreativeModeTab.Builder > > newTab( FMLJavaModLoadingContext context,
+		ResourceLocation location
+	) {
+		return builder->{
+			context.getModEventBus().addListener( ( CreativeModeTabEvent.Register event )->{
+				event.registerCreativeModeTab( location, builder );
+			} );
+		};
+	}
+
+	public static Supplier< ItemStack > buildMultiIcon( Stream< RegistryObject< ? extends Item > > items ) {
+		Function< RegistryObject< ? extends Item >, Supplier< ItemStack > > toItemStackSupplier = item->()->new ItemStack( item.get() );
+		List< Supplier< ItemStack > > itemStacks = items.map( toItemStackSupplier ).toList();
+		return ()->{
+			int idx = ( int )( ( TimeHelper.getClientTicks() / Utility.secondsToTicks( 5.0 ) ) ) % itemStacks.size();
+
+			return itemStacks.get( idx ).get();
+		};
+	}
+}
