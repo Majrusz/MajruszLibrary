9494d313fd632306d944c8c19fd9dbbadfaa2164
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-15 14:07:06 +0100

    Reworked AnnotationHandler

--- src/main/java/com/mlib/annotations/AnnotationHandler.java
+++ src/main/java/com/mlib/annotations/AnnotationHandler.java
@@ -1,36 +1,55 @@
 package com.mlib.annotations;
 
 import com.mlib.MajruszLibrary;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.forgespi.language.ModFileScanData;
+import org.objectweb.asm.Type;
 
-import java.io.File;
 import java.lang.annotation.Annotation;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.List;
 
 public class AnnotationHandler {
-	final List< Class< ? > > classes = new ArrayList<>();
-	final List< Object > instances = new ArrayList<>();
 	final Class< ? extends Annotation > annotationClass;
+	final List< ? extends Class< ? > > classes;
+	final List< ? > instances;
 
-	public AnnotationHandler( String rootPackage, Class< ? extends Annotation > annotationClass ) {
+	public AnnotationHandler( String packageName, Class< ? extends Annotation > annotationClass ) {
 		this.annotationClass = annotationClass;
-
-		try {
-			Enumeration< URL > resources = this.buildResources( rootPackage.replaceAll( "\\.", "/" ) );
-			while( resources.hasMoreElements() ) {
-				this.handle( new File( resources.nextElement().getPath() ) );
-			}
-			this.loadAllClasses();
-		} catch( AnnotationHandler.AnnotationException exception ) {
-			MajruszLibrary.logOnDev( "[AnnotationHandler] %s", exception.getMessage() );
-		}
+		Type type = Type.getType( annotationClass );
+		this.classes = ModList.get()
+			.getAllScanData()
+			.stream()
+			.map( ModFileScanData::getAnnotations )
+			.flatMap( Collection::stream )
+			.filter( annotationData->type.equals( annotationData.annotationType() ) )
+			.map( data->{
+				try {
+					Class< ? > clazz = Class.forName( data.clazz().getClassName() );
+					return clazz.getPackageName().contains( packageName ) ? clazz : null;
+				} catch( ClassNotFoundException e ) {
+					return null;
+				}
+			} )
+			.toList();
+		this.instances = this.classes.stream()
+			.map( clazz->{
+				try {
+					Object instance = clazz.getConstructor().newInstance();
+					MajruszLibrary.logOnDev( "[AnnotationHandler] Class %s has been initialized.", clazz.getCanonicalName() );
+					return instance;
+				} catch( Exception exception ) {
+					MajruszLibrary.logOnDev( "[AnnotationHandler] %s", exception.getMessage() );
+					return null;
+				}
+			} )
+			.toList();
 	}
 
-	public AnnotationHandler( String rootPackage ) {
-		this( rootPackage, AutoInstance.class );
+	public AnnotationHandler( String packageName ) {
+		this( packageName, AutoInstance.class );
 	}
 
 	public List< Class< ? > > getClasses() {
@@ -50,61 +69,5 @@ public class AnnotationHandler {
 		}
 
 		return instances;
-	}
-
-	private Enumeration< URL > buildResources( String rootPackage ) throws AnnotationException {
-		try {
-			return ClassLoader.getSystemResources( rootPackage );
-		} catch( Throwable ignored ) {
-			throw new AnnotationException( "Invalid package %s", rootPackage );
-		}
-	}
-
-	private void handle( File[] files ) {
-		for( File file : files ) {
-			try {
-				this.handle( file );
-			} catch( AnnotationHandler.AnnotationException exception ) {
-				MajruszLibrary.logOnDev( "[AnnotationHandler] %s", exception.getMessage() );
-			}
-		}
-	}
-
-	private void handle( File file ) throws AnnotationException {
-		String path = file.getPath();
-		if( file.isDirectory() ) {
-			this.handle( file.listFiles() );
-		} else if( file.getName().contains( ".class" ) ) {
-			String classPackage = path.replaceAll( ".*\\\\(com\\\\.*).class$", "$1" ).replaceAll( "\\\\", "." );
-			try {
-				Class< ? > _class = Class.forName( classPackage );
-				if( _class.isAnnotationPresent( this.annotationClass ) ) {
-					this.classes.add( _class );
-				}
-			} catch( Throwable ignored ) {
-				throw new AnnotationException( "Class %s has failed to load.", classPackage );
-			}
-		}
-	}
-
-	private void loadAllClasses() {
-		try {
-			for( Class< ? > _class : this.classes ) {
-				try {
-					this.instances.add( _class.getConstructor().newInstance() );
-					MajruszLibrary.logOnDev( "[AnnotationHandler] Class %s has been initialized.", _class.getCanonicalName() );
-				} catch( Throwable e ) {
-					throw new AnnotationException( "%s does not have an empty constructor", _class.getName() );
-				}
-			}
-		} catch( AnnotationHandler.AnnotationException exception ) {
-			MajruszLibrary.logOnDev( "[AnnotationHandler] %s", exception.getMessage() );
-		}
-	}
-
-	public static class AnnotationException extends Exception {
-		public AnnotationException( String format, Object... params ) {
-			super( String.format( format, params ) );
-		}
 	}
 }
