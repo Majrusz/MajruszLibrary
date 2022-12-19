9ec2e0c23200c98f6b7c949dd68b3b1ed8befef3
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-19 14:06:02 +0100

    Update Condition.java

--- src/main/java/com/mlib/gamemodifiers/Condition.java
+++ src/main/java/com/mlib/gamemodifiers/Condition.java
@@ -140,7 +140,7 @@ public abstract class Condition< DataType extends ContextData > extends ConfigGr
 	public static class ArmorDependentChance< DataType extends ContextData > extends Condition< DataType > {
 		static final Function< EquipmentSlot, String > SLOT_FORMAT = slot->String.format( "%s_multiplier", slot.getName() );
 		final Map< EquipmentSlot, DoubleConfig > multipliers = new HashMap<>();
-		final ConfigGroup group = new ConfigGroup( "ArmorChanceMultipliers", "Chance multipliers for each armor piece.\nFor instance 'head_multiplier = 0.9' makes the final chance 30% lower if the mob has any helmet." );
+		final ConfigGroup group = new ConfigGroup( "ArmorChanceMultipliers", "Chance multipliers for each armor piece.\nFor instance 'head_multiplier = 0.8' makes the final chance 30% lower if the mob has any helmet." );
 
 		public ArmorDependentChance( Map< EquipmentSlot, Double > chances ) {
 			for( EquipmentSlot slot : EquipmentSlots.ARMOR ) {
