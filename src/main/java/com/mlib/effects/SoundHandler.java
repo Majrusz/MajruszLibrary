b1afa5f7b6743564fb3e0f0eba2a868a9883d88f
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-21 18:11:23 +0100

    Added possibility to use RegistryObject

--- src/main/java/com/mlib/effects/SoundHandler.java
+++ src/main/java/com/mlib/effects/SoundHandler.java
@@ -1,12 +1,12 @@
 package com.mlib.effects;
 
 import com.mlib.Random;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
@@ -23,7 +23,7 @@ public class SoundHandler {
 	public static final SoundHandler ITEM_PICKUP = new SoundHandler( SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS );
 	public static final SoundHandler SMELT = new SoundHandler( SoundEvents.FIRECHARGE_USE, SoundSource.AMBIENT );
 
-	final SoundEvent event;
+	final Supplier< SoundEvent > event;
 	final SoundSource source;
 	final Supplier< Float > volumeProvider;
 	final Supplier< Float > pitchProvider;
@@ -32,15 +32,33 @@ public class SoundHandler {
 		return ()->value * Random.nextFloat( 0.8f, 1.2f );
 	}
 
-	public SoundHandler( SoundEvent event, SoundSource source, Supplier< Float > volumeProvider, Supplier< Float > pitchProvider ) {
+	public SoundHandler( RegistryObject< SoundEvent > event, SoundSource source, Supplier< Float > volumeProvider,
+		Supplier< Float > pitchProvider
+	) {
 		this.event = event;
 		this.source = source;
 		this.volumeProvider = volumeProvider;
 		this.pitchProvider = pitchProvider;
 	}
 
+	public SoundHandler( SoundEvent event, SoundSource source, Supplier< Float > volumeProvider, Supplier< Float > pitchProvider
+	) {
+		this.event = ()->event;
+		this.source = source;
+		this.volumeProvider = volumeProvider;
+		this.pitchProvider = pitchProvider;
+	}
+
+	public SoundHandler( RegistryObject< SoundEvent > event, SoundSource source, Supplier< Float > volumeProvider ) {
+		this( event, source, volumeProvider, DEFAULT_PITCH );
+	}
+
 	public SoundHandler( SoundEvent event, SoundSource source, Supplier< Float > volumeProvider ) {
 		this( event, source, volumeProvider, DEFAULT_PITCH );
+	}
+
+	public SoundHandler( RegistryObject< SoundEvent > event, SoundSource source ) {
+		this( event, source, DEFAULT_VOLUME, DEFAULT_PITCH );
 	}
 
 	public SoundHandler( SoundEvent event, SoundSource source ) {
@@ -56,6 +74,6 @@ public class SoundHandler {
 	}
 
 	public void play( Level level, Vec3 position, Supplier< Float > volumeProvider, Supplier< Float > pitchProvider ) {
-		level.playSound( null, position.x, position.y, position.z, this.event, this.source, volumeProvider.get(), pitchProvider.get() );
+		level.playSound( null, position.x, position.y, position.z, this.event.get(), this.source, volumeProvider.get(), pitchProvider.get() );
 	}
 }
