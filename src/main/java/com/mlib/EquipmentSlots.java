1ae2774dbdc9eab924c2f844a926d0ff4156fd1d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-23 18:29:51 +0100

    Code refactored

--- src/main/java/com/mlib/EquipmentSlots.java
+++ src/main/java/com/mlib/EquipmentSlots.java
@@ -4,14 +4,25 @@ import net.minecraft.world.entity.EquipmentSlot;
 
 public class EquipmentSlots {
 	public static EquipmentSlot[] ARMOR = new EquipmentSlot[]{
-		EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET
+		EquipmentSlot.HEAD,
+		EquipmentSlot.CHEST,
+		EquipmentSlot.LEGS,
+		EquipmentSlot.FEET
 	};
 	public static EquipmentSlot[] BOTH_HANDS = new EquipmentSlot[]{
-		EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND
+		EquipmentSlot.MAINHAND,
+		EquipmentSlot.OFFHAND
 	};
-	public static EquipmentSlot[] ARMOR_AND_HANDS = new EquipmentSlot[]{
-		EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND
+	public static EquipmentSlot[] ALL = new EquipmentSlot[]{
+		EquipmentSlot.HEAD,
+		EquipmentSlot.CHEST,
+		EquipmentSlot.LEGS,
+		EquipmentSlot.FEET,
+		EquipmentSlot.MAINHAND,
+		EquipmentSlot.OFFHAND
 	};
+	@Deprecated
+	public static EquipmentSlot[] ARMOR_AND_HANDS = ALL;
 	// below are default values wrapped into arrays
 	public static EquipmentSlot[] MAINHAND = new EquipmentSlot[]{ EquipmentSlot.MAINHAND };
 	public static EquipmentSlot[] OFFHAND = new EquipmentSlot[]{ EquipmentSlot.OFFHAND };
