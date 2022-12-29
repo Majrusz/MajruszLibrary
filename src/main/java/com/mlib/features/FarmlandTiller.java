606d7acb94aa088d033c2c95168f865e2f7fd35f
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-29 19:40:31 +0100

    Replaced event with context

--- src/main/java/com/mlib/features/FarmlandTiller.java
+++ src/main/java/com/mlib/features/FarmlandTiller.java
@@ -1,9 +1,9 @@
 package com.mlib.features;
 
 import com.mlib.annotations.AutoInstance;
-import com.mlib.events.FarmlandTillCheckEvent;
 import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.GameModifier;
+import com.mlib.gamemodifiers.contexts.OnFarmlandTillCheck;
 import com.mlib.gamemodifiers.contexts.OnPlayerInteract;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -13,9 +13,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.common.MinecraftForge;
 
-import javax.annotation.Nullable;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -25,22 +23,21 @@ public class FarmlandTiller extends GameModifier {
 	final Function< OnPlayerInteract.Data, Player > PLAYER_SUPPLIER = data->data.player;
 
 	public FarmlandTiller() {
-		OnPlayerInteract.Context onInteract = new OnPlayerInteract.Context( this::applyHoeBonus );
-		onInteract.addCondition( new Condition.IsServer<>() )
+		new OnPlayerInteract.Context( this::applyHoeBonus )
+			.addCondition( new Condition.IsServer<>() )
 			.addCondition( new Condition.IsShiftKeyDown<>( PLAYER_SUPPLIER ).negate() )
 			.addCondition( OnPlayerInteract.IS_BLOCK_INTERACTION )
-			.addCondition( OnPlayerInteract.HAS_FACE );
-
-		this.addContext( onInteract );
+			.addCondition( OnPlayerInteract.HAS_FACE )
+			.insertTo( this );
 	}
 
 	private void applyHoeBonus( OnPlayerInteract.Data data ) {
-		FarmlandTillCheckEvent event = this.dispatchEvent( data );
-		if( event == null || this.getInfo( data.level, data.player, data.position, data.hand, data.face ) == null )
+		OnFarmlandTillCheck.Data extraData = this.dispatchContext( data );
+		if( extraData.area == 0 || this.getInfo( data.level, data.player, data.position, data.hand, data.face ) == null )
 			return;
 
-		for( int x = -event.area; x <= event.area; ++x ) {
-			for( int z = -event.area; z <= event.area; ++z ) {
+		for( int x = -extraData.area; x <= extraData.area; ++x ) {
+			for( int z = -extraData.area; z <= extraData.area; ++z ) {
 				BlockInfo blockInfo = this.getInfo( data.level, data.player, data.position.offset( x, 0, z ), data.hand, data.face );
 				if( blockInfo != null ) {
 					blockInfo.accept();
@@ -50,11 +47,8 @@ public class FarmlandTiller extends GameModifier {
 		}
 	}
 
-	@Nullable
-	private FarmlandTillCheckEvent dispatchEvent( OnPlayerInteract.Data data ) {
-		FarmlandTillCheckEvent event = new FarmlandTillCheckEvent( data.level, data.player, data.itemStack );
-
-		return !MinecraftForge.EVENT_BUS.post( event ) && event.area > 0 ? event : null;
+	private OnFarmlandTillCheck.Data dispatchContext( OnPlayerInteract.Data data ) {
+		return OnFarmlandTillCheck.Context.accept( new OnFarmlandTillCheck.Data( data ) );
 	}
 
 	private BlockInfo getInfo( ServerLevel level, Player player, BlockPos position, InteractionHand hand, Direction direction ) {
--- src/main/java/com/mlib/gamemodifiers/contexts/OnFarmlandTillCheck.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnFarmlandTillCheck.java
@@ -1,45 +1,47 @@
 package com.mlib.gamemodifiers.contexts;
 
-import com.mlib.events.FarmlandTillCheckEvent;
 import com.mlib.gamemodifiers.ContextBase;
 import com.mlib.gamemodifiers.ContextData;
 import com.mlib.gamemodifiers.Contexts;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class OnFarmlandTillCheck {
-	public static final Consumer< Data > INCREASE_AREA = data->++data.event.area;
+	public static final Consumer< Data > INCREASE_AREA = data->++data.area;
 	public static final Predicate< Data > IS_NOT_CROUCHING = data->!data.player.isCrouching();
 
-	@Mod.EventBusSubscriber
 	public static class Context extends ContextBase< Data > {
 		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public static Data accept( Data data ) {
+			CONTEXTS.accept( data );
+
+			return data;
+		}
 
 		public Context( Consumer< Data > consumer ) {
 			super( consumer );
 
 			CONTEXTS.add( this );
 		}
-
-		@SubscribeEvent
-		public static void onCheck( FarmlandTillCheckEvent event ) {
-			CONTEXTS.accept( new Data( event ) );
-		}
 	}
 
-	public static class Data extends ContextData.Event< FarmlandTillCheckEvent > {
+	public static class Data extends ContextData {
+		public int area = 0;
+		public final ServerLevel level;
 		public final Player player;
 		public final ItemStack itemStack;
 
-		public Data( FarmlandTillCheckEvent event ) {
-			super( event.player, event );
-			this.player = event.player;
-			this.itemStack = event.itemStack;
+		public Data( OnPlayerInteract.Data data ) {
+			super( data.player );
+
+			this.level = data.level;
+			this.player = data.player;
+			this.itemStack = data.itemStack;
 		}
 	}
 }
