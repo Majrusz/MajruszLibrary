385e21e8496bea8a976bdcd072f3138642365315
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-30 16:10:50 +0100

    Reworked OnPreDamaged to allow for changing the damage value

--- src/main/java/com/mlib/gamemodifiers/contexts/OnPreDamaged.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnPreDamaged.java
@@ -5,60 +5,49 @@ import com.mlib.gamemodifiers.ContextBase;
 import com.mlib.gamemodifiers.ContextData;
 import com.mlib.gamemodifiers.Contexts;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class OnPreDamaged {
-	public static final Consumer< Data > CANCEL = data->data.event.setCanceled( true );
+	public static final Consumer< Data > CANCEL = data->data.isCancelled = true;
 	public static final Predicate< Data > DIRECT_DAMAGE = data->data.source.getDirectEntity() == data.attacker;
-	public static final Predicate< Data > DEALT_ANY_DAMAGE = data->data.event.getAmount() > 0.0f;
+	public static final Predicate< Data > DEALT_ANY_DAMAGE = data->data.damage > 0.0f;
 	public static final Predicate< Data > WILL_TAKE_FULL_DAMAGE = data->data.target.invulnerableTime <= 10; // sources like fire deal damage every tick and only invulnerableTime blocks them from applying damage
 
-	@Mod.EventBusSubscriber
 	public static class Context extends ContextBase< Data > {
 		static final Contexts< Data, Context > CONTEXTS = new Contexts<>();
+
+		public static Data accept( Data data ) {
+			CONTEXTS.accept( data );
+
+			return data;
+		}
 
 		public Context( Consumer< Data > consumer ) {
 			super( consumer );
 
 			CONTEXTS.add( this );
 		}
-
-		@SubscribeEvent
-		public static void onPreDamaged( LivingAttackEvent event ) {
-			Data data = new Data( event );
-			if( !willBeCancelled( data ) ) {
-				CONTEXTS.accept( data );
-			}
-		}
-
-		private static boolean willBeCancelled( Data data ) {
-			boolean isInvulnerable = data.target.isInvulnerableTo( data.source );
-			boolean isClientSide = data.level == null;
-			boolean isDeadOrDying = data.target.isDeadOrDying();
-			boolean isFireResistant = data.source.isFire() && data.target.hasEffect( MobEffects.FIRE_RESISTANCE );
-
-			return isInvulnerable || isClientSide || isDeadOrDying || isFireResistant;
-		}
 	}
 
-	public static class Data extends ContextData.Event< LivingAttackEvent > {
+	public static class Data extends ContextData {
 		public final DamageSource source;
 		@Nullable public final LivingEntity attacker;
 		public final LivingEntity target;
+		public final float damage;
+		public float extraDamage = 0;
+		public boolean isCancelled = false;
 
-		public Data( LivingAttackEvent event ) {
-			super( event.getEntity(), event );
-			this.source = event.getSource();
-			this.attacker = Utility.castIfPossible( LivingEntity.class, source.getEntity() );
-			this.target = event.getEntity();
+		public Data( DamageSource source, LivingEntity target, float damage ) {
+			super( target );
+
+			this.source = source;
+			this.attacker = Utility.castIfPossible( LivingEntity.class, this.source.getEntity() );
+			this.target = target;
+			this.damage = damage;
 		}
 	}
 }
--- src/main/java/com/mlib/mixin/MixinLivingEntity.java
+++ src/main/java/com/mlib/mixin/MixinLivingEntity.java
@@ -1,16 +1,22 @@
 package com.mlib.mixin;
 
 import com.mlib.events.ItemSwingDurationEvent;
+import com.mlib.gamemodifiers.contexts.OnPreDamaged;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin( LivingEntity.class )
 public abstract class MixinLivingEntity {
+	float mlibLastExtraDamage = 0.0f;
+
 	@Shadow( aliases = { "this$0" } )
 	@Inject( method = "getCurrentSwingDuration ()I", at = @At( "RETURN" ), cancellable = true )
 	private void getCurrentSwingDuration( CallbackInfoReturnable< Integer > callback ) {
@@ -19,5 +25,36 @@ public abstract class MixinLivingEntity {
 		MinecraftForge.EVENT_BUS.post( event );
 
 		callback.setReturnValue( event.getTotalSwingDuration() );
+	}
+
+	@Shadow( aliases = { "this$0" } )
+	@ModifyVariable( method = "hurt (Lnet/minecraft/world/damagesource/DamageSource;F)Z", at = @At( value = "HEAD", shift = At.Shift.AFTER ), ordinal = 0 )
+	private float addExtraDamage( float damage ) {
+		return damage + this.mlibLastExtraDamage;
+	}
+
+	@Shadow( aliases = { "this$0" } )
+	@Inject( method = "hurt (Lnet/minecraft/world/damagesource/DamageSource;F)Z", at = @At( "HEAD" ), cancellable = true )
+	private void hurt( DamageSource source, float damage, CallbackInfoReturnable< Boolean > callback ) {
+		this.mlibLastExtraDamage = 0.0f;
+		OnPreDamaged.Data data = new OnPreDamaged.Data( source, ( LivingEntity )( Object )this, damage );
+		if( willBeCancelled( data ) )
+			return;
+
+		OnPreDamaged.Context.accept( data );
+		if( data.isCancelled ) {
+			callback.setReturnValue( false );
+		} else {
+			this.mlibLastExtraDamage = data.extraDamage;
+		}
+	}
+
+	private static boolean willBeCancelled( OnPreDamaged.Data data ) {
+		boolean isInvulnerable = data.target.isInvulnerableTo( data.source );
+		boolean isClientSide = data.level == null;
+		boolean isDeadOrDying = data.target.isDeadOrDying();
+		boolean isFireResistant = data.source.isFire() && data.target.hasEffect( MobEffects.FIRE_RESISTANCE );
+
+		return isInvulnerable || isClientSide || isDeadOrDying || isFireResistant;
 	}
 }
