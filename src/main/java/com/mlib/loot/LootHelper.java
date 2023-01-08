a8329ac6d2497bce6edf663491048514a2aa6837
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-01-08 15:20:53 +0100

    Added LootHelper to handle common loot usage

---
+++ src/main/java/com/mlib/loot/LootHelper.java
@@ -0,0 +1,23 @@
+package com.mlib.loot;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
+public class LootHelper {
+	public LootTable getLootTable( ResourceLocation location ) {
+		return ServerLifecycleHooks.getCurrentServer().getLootTables().get( location );
+	}
+
+	public LootContext toGiftContext( Entity entity ) {
+		return new LootContext.Builder( ( ServerLevel )entity.level )
+			.withParameter( LootContextParams.ORIGIN, entity.position() )
+			.withParameter( LootContextParams.THIS_ENTITY, entity )
+			.create( LootContextParamSets.GIFT );
+	}
+}
