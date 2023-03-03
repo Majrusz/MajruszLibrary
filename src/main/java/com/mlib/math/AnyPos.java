9284cca717e851e6a3c9e4cd2fda0085bf5b4a5a
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-03-03 22:46:40 +0100

    Added add() functions

--- src/main/java/com/mlib/math/AnyPos.java
+++ src/main/java/com/mlib/math/AnyPos.java
@@ -34,6 +34,30 @@ public class AnyPos {
 		this.z = z;
 	}
 
+	public AnyPos add( Vec3 vec3 ) {
+		return add( this, from( vec3 ) );
+	}
+
+	public AnyPos add( Vector3f vec3f ) {
+		return add( this, from( vec3f ) );
+	}
+
+	public AnyPos add( Vec3i vec3i ) {
+		return add( this, from( vec3i ) );
+	}
+
+	public AnyPos add( BlockPos blockPos ) {
+		return add( this, from( blockPos ) );
+	}
+
+	public AnyPos add( Number value ) {
+		return add( this, from( value, value, value ) );
+	}
+
+	public AnyPos add( Number x, Number y, Number z ) {
+		return add( this, from( x, y, z ) );
+	}
+
 	public Vec3 vec3() {
 		return new Vec3( this.x, this.y, this.z );
 	}
@@ -48,5 +72,9 @@ public class AnyPos {
 
 	public BlockPos block() {
 		return new BlockPos( ( int )this.x, ( int )this.y, ( int )this.z );
+	}
+
+	private static AnyPos add( AnyPos pos1, AnyPos pos2 ) {
+		return new AnyPos( pos1.x + pos2.x, pos1.y + pos2.y, pos1.z + pos2.z );
 	}
 }
--- src/main/java/com/mlib/tests/AnyPosTests.java
+++ src/main/java/com/mlib/tests/AnyPosTests.java
@@ -17,21 +17,44 @@ public class AnyPosTests extends BaseTest {
 	@GameTest( templateNamespace = "mlib", template = "empty_test" )
 	public static void canonicity( GameTestHelper helper ) {
 		Supplier< String > testMessage = ()->"AnyPos is not canonical (values passed to AnyPos are different from those returned)";
-		Vec3 vec3 = new Vec3( 0.0, 0.1, -0.9 );
-		Vector3f vec3f = new Vector3f( -0.1f, 0.2f, -0.3f );
-		Vec3i vec3i = new Vec3i( 0, 3, 4 );
-		BlockPos blockPos = new BlockPos( -1, 4, 23 );
-		double x = 1.0;
-		float y = 0.4f;
-		int z = 3;
 
-		assertThat( helper, AnyPos.from( vec3 ).vec3(), vec3, testMessage );
-		assertThat( helper, AnyPos.from( vec3f ).vec3f(), vec3f, testMessage );
-		assertThat( helper, AnyPos.from( vec3i ).vec3i(), vec3i, testMessage );
-		assertThat( helper, AnyPos.from( blockPos ).block(), blockPos, testMessage );
-		assertThat( helper, AnyPos.from( x, y, z ).vec3(), new Vec3( x, y, z ), testMessage );
+		assertThat( helper, AnyPos.from( new Vec3( 0.0, 0.1, -0.9 ) ), new Vec3( 0.0, 0.1, -0.9 ), testMessage );
+		assertThat( helper, AnyPos.from( new Vector3f( -0.1f, 0.2f, -0.3f ) ), new Vector3f( -0.1f, 0.2f, -0.3f ), testMessage );
+		assertThat( helper, AnyPos.from( new Vec3i( 0, 3, 4 ) ), new Vec3i( 0, 3, 4 ), testMessage );
+		assertThat( helper, AnyPos.from( new BlockPos( -1, 4, 23 ) ), new BlockPos( -1, 4, 23 ), testMessage );
+		assertThat( helper, AnyPos.from( 1.0, 0.4f, 3 ), new Vec3( 1.0, 0.4f, 3 ), testMessage );
 
 		helper.succeed();
+	}
+
+	@GameTest( templateNamespace = "mlib", template = "empty_test" )
+	public static void add( GameTestHelper helper ) {
+		Supplier< String > testMessage = ()->"AnyPos does not add objects properly";
+
+		assertThat( helper, AnyPos.from( 0.0, 0.1, 0.2 ).add( new Vec3( 0.2, 0.1, 0.0 ) ), new Vec3( 0.2, 0.2, 0.2 ), testMessage );
+		assertThat( helper, AnyPos.from( 1.0f, -0.5f, -1.0f ).add( new Vector3f( -1.0f, 0.5f, 1.0f ) ), new Vector3f( 0.0f, 0.0f, 0.0f ), testMessage );
+		assertThat( helper, AnyPos.from( 0, 1, 0 ).add( new Vec3i( 1, 2, 3 ) ), new Vec3i( 1, 3, 3 ), testMessage );
+		assertThat( helper, AnyPos.from( -1, 0, 1 ).add( new BlockPos( -1, 0, 1 ) ), new BlockPos( -2, 0, 2 ), testMessage );
+		assertThat( helper, AnyPos.from( 0, 0, 0 ).add( 1 ), new Vec3( 1, 1, 1 ), testMessage );
+		assertThat( helper, AnyPos.from( 0, 0, 0 ).add( 1, 2, 3 ), new Vec3( 1, 2, 3 ), testMessage );
+
+		helper.succeed();
+	}
+
+	public static void assertThat( GameTestHelper helper, AnyPos result, Vec3 expected, Supplier< String > message ) {
+		assertThat( helper, result.vec3(), expected, message );
+	}
+
+	public static void assertThat( GameTestHelper helper, AnyPos result, Vector3f expected, Supplier< String > message ) {
+		assertThat( helper, result.vec3f(), expected, message );
+	}
+
+	public static void assertThat( GameTestHelper helper, AnyPos result, Vec3i expected, Supplier< String > message ) {
+		assertThat( helper, result.vec3i(), expected, message );
+	}
+
+	public static void assertThat( GameTestHelper helper, AnyPos result, BlockPos expected, Supplier< String > message ) {
+		assertThat( helper, result.block(), expected, message );
 	}
 
 	public AnyPosTests() {
