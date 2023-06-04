a637bafb65b00e58a00a7729bac4a7bf2693f514
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-06-04 13:03:13 +0200

    Fixed bug with OnTradeSetup not working properly

--- src/main/java/com/mlib/gamemodifiers/contexts/OnTradeSetup.java
+++ src/main/java/com/mlib/gamemodifiers/contexts/OnTradeSetup.java
@@ -6,10 +6,12 @@ import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.List;
 import java.util.function.Consumer;
 
+@Mod.EventBusSubscriber
 public class OnTradeSetup {
 	public static Context< Data > listen( Consumer< Data > consumer ) {
 		return Contexts.get( Data.class ).add( consumer );
