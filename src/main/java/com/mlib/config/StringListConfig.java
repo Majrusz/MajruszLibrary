33fda4232d39fdd34426cb4f3fdcb04c1c9a4d4d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-06-21 19:59:04 +0200

    Added regex support for StringListConfig

--- src/main/java/com/mlib/config/StringListConfig.java
+++ src/main/java/com/mlib/config/StringListConfig.java
@@ -2,10 +2,15 @@ package com.mlib.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class StringListConfig extends ValueConfig< List< ? extends String > > {
+	public static final String REGEX_PREFIX = "{regex}";
+	protected final List< Predicate< String > > predicates = new ArrayList<>();
+
 	public StringListConfig( List< ? extends String > defaultValue ) {
 		super( defaultValue );
 	}
@@ -21,7 +26,28 @@ public class StringListConfig extends ValueConfig< List< ? extends String > > {
 		this.config = builder.defineList( this.name, this.defaultValue, list->true );
 	}
 
+	@Override
+	public void onReload() {
+		super.onReload();
+
+		this.predicates.clear();
+		this.getOrDefault()
+			.forEach( value->{
+				if( value.startsWith( REGEX_PREFIX ) ) {
+					this.predicates.add( subvalue->subvalue.matches( value.substring( REGEX_PREFIX.length() ) ) );
+				} else {
+					this.predicates.add( subvalue->subvalue.equals( value ) );
+				}
+			} );
+	}
+
 	public boolean contains( String value ) {
-		return this.config.get().contains( value );
+		for( Predicate< String > predicate : this.predicates ) {
+			if( predicate.test( value ) ) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
