191032c82c152e12e0ad6a6a945c4411241f08e5
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-06-30 15:14:49 +0200

    Added possibility to sync custom data packs with clients on server

--- src/main/java/com/mlib/data/JsonListener.java
+++ src/main/java/com/mlib/data/JsonListener.java
@@ -2,6 +2,7 @@ package com.mlib.data;
 
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.mlib.modhelper.ModHelper;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
@@ -9,6 +10,8 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.storage.loot.Deserializers;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.event.OnDatapackSyncEvent;
+import net.minecraftforge.network.PacketDistributor;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,26 +22,53 @@ import java.util.function.Supplier;
 public class JsonListener {
 	final static List< SerializableStructure > STRUCTURES = Collections.synchronizedList( new ArrayList<>() );
 
-	public static < Type extends SerializableStructure > Supplier< Type > add( String data, ResourceLocation location, Class< Type > clazz,
+	public static < Type extends SerializableStructure > Holder< Type > add( String data, ResourceLocation location, Class< Type > clazz,
 		Supplier< Type > newInstance
 	) {
-		int idx = STRUCTURES.size();
-		STRUCTURES.add( null );
+		Holder< Type > holder = new Holder<>( STRUCTURES.size() );
+		STRUCTURES.add( newInstance.get() );
 		var gson = Deserializers.createFunctionSerializer()
-			.registerTypeAdapter( clazz, ( JsonDeserializer< Type > )( element, type, context )->{
-				Type messages = newInstance.get();
-				messages.read( element );
-
-				return messages;
-			} ).create();
+			.registerTypeAdapter( clazz, ( JsonDeserializer< Type > )( element, type, context )->SerializableHelper.read( newInstance, element ) )
+			.create();
 		var listener = new SimpleJsonResourceReloadListener( gson, data ) {
 			@Override
 			protected void apply( Map< ResourceLocation, JsonElement > elements, ResourceManager manager, ProfilerFiller filler ) {
-				STRUCTURES.set( idx, gson.fromJson( elements.get( location ), clazz ) );
+				STRUCTURES.set( holder.idx, gson.fromJson( elements.get( location ), clazz ) );
 			}
 		};
 		MinecraftForge.EVENT_BUS.addListener( ( AddReloadListenerEvent event )->event.addListener( listener ) );
 
-		return ()->( Type )STRUCTURES.get( idx );
+		return holder;
+	}
+
+	public static class Holder< Type extends SerializableStructure > implements Supplier< Type > {
+		final int idx;
+
+		Holder( int idx ) {
+			this.idx = idx;
+		}
+
+		public Holder< Type > syncWithClients( ModHelper helper ) {
+			MinecraftForge.EVENT_BUS.addListener( ( OnDatapackSyncEvent event )->{
+				if( event.getPlayer() != null ) {
+					helper.sendMessage( PacketDistributor.PLAYER.with( event::getPlayer ), this.get() );
+				}
+			} );
+
+			return this;
+		}
+
+		public void onSync( Type instance ) {
+			STRUCTURES.set( this.idx, instance );
+		}
+
+		@Override
+		public Type get() {
+			return ( Type )STRUCTURES.get( this.idx );
+		}
+
+		public boolean isPresent() {
+			return this.get() != null;
+		}
 	}
 }
