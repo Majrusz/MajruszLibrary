6f8fb29a3cca0d598c3f68400f517ceee989184d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-09-08 17:06:11 +0200

    Moved EquipmentSlots to items package

---
+++ src/main/java/com/mlib/items/EquipmentSlots.java
@@ -0,0 +1,30 @@
+package com.mlib.items;
+
+import net.minecraft.world.entity.EquipmentSlot;
+
+public class EquipmentSlots {
+	public static final EquipmentSlot[] ARMOR = new EquipmentSlot[]{
+		EquipmentSlot.HEAD,
+		EquipmentSlot.CHEST,
+		EquipmentSlot.LEGS,
+		EquipmentSlot.FEET
+	};
+	public static final EquipmentSlot[] BOTH_HANDS = new EquipmentSlot[]{
+		EquipmentSlot.MAINHAND,
+		EquipmentSlot.OFFHAND
+	};
+	public static final EquipmentSlot[] ALL = new EquipmentSlot[]{
+		EquipmentSlot.HEAD,
+		EquipmentSlot.CHEST,
+		EquipmentSlot.LEGS,
+		EquipmentSlot.FEET,
+		EquipmentSlot.MAINHAND,
+		EquipmentSlot.OFFHAND
+	};
+	public static final EquipmentSlot[] MAINHAND = new EquipmentSlot[]{ EquipmentSlot.MAINHAND };
+	public static final EquipmentSlot[] OFFHAND = new EquipmentSlot[]{ EquipmentSlot.OFFHAND };
+	public static final EquipmentSlot[] HEAD = new EquipmentSlot[]{ EquipmentSlot.HEAD };
+	public static final EquipmentSlot[] CHEST = new EquipmentSlot[]{ EquipmentSlot.CHEST };
+	public static final EquipmentSlot[] LEGS = new EquipmentSlot[]{ EquipmentSlot.LEGS };
+	public static final EquipmentSlot[] FEET = new EquipmentSlot[]{ EquipmentSlot.FEET };
+}
