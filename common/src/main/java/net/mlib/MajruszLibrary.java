51066dacd08d5bea6810cc050045dd7ba04fd2b9
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-09-25 20:03:06 +0200

    Added test item and creative mode tab

--- common/src/main/java/net/mlib/MajruszLibrary.java
+++ common/src/main/java/net/mlib/MajruszLibrary.java
@@ -13,5 +13,22 @@ public class MajruszLibrary {
 	public static final String MOD_ID = "mlib";
 	public static final ModHelper HELPER = ModHelper.create( MOD_ID );
 
+	// Registry Groups
+	public static final RegistryGroup< Item > ITEMS = HELPER.getRegistryHandler().create( BuiltInRegistries.ITEM );
+	public static final RegistryGroup< CreativeModeTab > CREATIVE_MODE_TABS = HELPER.getRegistryHandler().create( BuiltInRegistries.CREATIVE_MODE_TAB );
+
+	// Items
+	public static final RegistryObject< Item > THE_ITEM = ITEMS.create( "the_item", ()->new Item( new Item.Properties() ) );
+
+	// Creative Mode Tabs
+	public static final RegistryObject< CreativeModeTab > THE_TAB = CREATIVE_MODE_TABS.create( "the_tab", ()->{
+		return CreativeModeTab.builder( CreativeModeTab.Row.TOP, 0 )
+			// .withTabsBefore( ResourceKey.create( Registries.CREATIVE_MODE_TAB, new ResourceLocation( "spawn_eggs" ) ) )
+			.title( Component.translatable( "itemGroup.mlib.the_tab" ) )
+			.icon( ()->new ItemStack( THE_ITEM.get() ) )
+			.displayItems( ( params, output )->output.accept( new ItemStack( THE_ITEM.get() ) ) )
+			.build();
+	} );
+
 	private MajruszLibrary() {}
 }
