4741f05d6d154789df835ce5262e67edcddf3370
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-09-30 12:25:39 +0200

    Added base conditions

--- common/src/main/java/net/mlib/contexts/base/Condition.java
+++ common/src/main/java/net/mlib/contexts/base/Condition.java
@@ -1,13 +1,24 @@
 package net.mlib.contexts.base;
 
+import net.mlib.MajruszLibrary;
+
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Condition< DataType > {
 	final Predicate< DataType > predicate;
 	boolean isNegated = false;
 
-	public Condition( Predicate< DataType > predicate ) {
-		this.predicate = predicate;
+	public static < DataType > Condition< DataType > isServer() {
+		return new Condition< DataType >( data->MajruszLibrary.SIDE.isServer() );
+	}
+
+	public static < DataType > Condition< DataType > predicate( Predicate< DataType > predicate ) {
+		return new Condition< DataType >( predicate );
+	}
+
+	public static < DataType > Condition< DataType > predicate( Supplier< Boolean > check ) {
+		return new Condition< DataType >( data->check.get() );
 	}
 
 	public Condition< DataType > negate() {
@@ -22,5 +33,9 @@ public class Condition< DataType > {
 
 	public boolean check( DataType data ) {
 		return this.isNegated ^ this.predicate.test( data );
+	}
+
+	private Condition( Predicate< DataType > predicate ) {
+		this.predicate = predicate;
 	}
 }
