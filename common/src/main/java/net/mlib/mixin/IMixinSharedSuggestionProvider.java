5fe3a5d8e8fbf12b0e0a5396e61039f1dfda0004
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-09-30 19:14:39 +0200

    Added fix for suggestion providers

---
+++ common/src/main/java/net/mlib/mixin/IMixinSharedSuggestionProvider.java
@@ -0,0 +1,29 @@
+package net.mlib.mixin;
+
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Mixin( SharedSuggestionProvider.class )
+public interface IMixinSharedSuggestionProvider {
+	@Overwrite
+	static < Type > void filterResources( Iterable< Type > registries, String command, Function< Type, ResourceLocation > mapper,
+		Consumer< Type > suggestions
+	) {
+		boolean containsColon = command.indexOf( 58 ) > -1;
+		for( Type registry : registries ) {
+			ResourceLocation id = mapper.apply( registry );
+			if( containsColon ) {
+				if( SharedSuggestionProvider.matchesSubStr( command, id.toString() ) ) {
+					suggestions.accept( registry );
+				}
+			} else if( SharedSuggestionProvider.matchesSubStr( command, id.getNamespace() ) || SharedSuggestionProvider.matchesSubStr( command, id.getPath() ) ) {
+				suggestions.accept( registry );
+			}
+		}
+	}
+}
