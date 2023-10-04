81ca524eedd77af5aa7f87e2542dc76e0ac4c97f
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-04 13:41:42 +0200

    Added OnEnderManAngered context

---
+++ common/src/main/java/com/mlib/temp/EnderManLeatherProtector.java
@@ -0,0 +1,15 @@
+package com.mlib.temp;
+
+import com.mlib.annotations.AutoInstance;
+import com.mlib.contexts.OnEnderManAngered;
+import com.mlib.contexts.base.Condition;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Items;
+
+@AutoInstance
+public class EnderManLeatherProtector {
+	public EnderManLeatherProtector() {
+		OnEnderManAngered.listen( OnEnderManAngered::cancelAnger )
+			.addCondition( Condition.predicate( data->data.player.getItemBySlot( EquipmentSlot.HEAD ).is( Items.TURTLE_HELMET ) ) );
+	}
+}
---
+++ fabric/src/main/java/com/mlib/mixin/fabric/MixinEnderMan.java
@@ -0,0 +1,42 @@
+package com.mlib.mixin.fabric;
+
+import com.mlib.contexts.OnEnderManAngered;
+import com.mlib.contexts.base.Contexts;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin( EnderMan.class )
+public abstract class MixinEnderMan {
+	private Player mlibLastPlayer = null;
+
+	@Inject(
+		at = @At( "HEAD" ),
+		method = "isLookingAtMe (Lnet/minecraft/world/entity/player/Player;)Z"
+	)
+	private void isLookingAtMe( Player player, CallbackInfoReturnable< Boolean > callback ) {
+		this.mlibLastPlayer = player;
+	}
+
+	@Redirect(
+		at = @At(
+			target = "Lnet/minecraft/world/item/ItemStack;is (Lnet/minecraft/world/item/Item;)Z",
+			value = "INVOKE"
+		),
+		method = "isLookingAtMe (Lnet/minecraft/world/entity/player/Player;)Z"
+	)
+	private boolean is( ItemStack itemStack, Item item ) {
+		EnderMan enderMan = ( EnderMan )( Object )this;
+
+		return itemStack.is( item )
+			|| Contexts.dispatch( new OnEnderManAngered( enderMan, this.mlibLastPlayer ) ).isAngerCancelled();
+	}
+}
