466c3ec0af71c1f4b93bd732b273042a86684e4d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-06 17:29:58 +0200

    Added OnItemBrewed context

---
+++ common/src/main/java/com/mlib/temp/ReplacePotions.java
@@ -0,0 +1,15 @@
+package com.mlib.temp;
+
+import com.mlib.annotations.AutoInstance;
+import com.mlib.contexts.OnItemBrewed;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+@AutoInstance
+public class ReplacePotions {
+	public ReplacePotions() {
+		OnItemBrewed.listen( data->data.mapPotions( itemStacks->itemStacks.stream().map( itemStack->{
+			return itemStack.isEmpty() ? itemStack : new ItemStack( Items.APPLE );
+		} ).toList() ) );
+	}
+}
