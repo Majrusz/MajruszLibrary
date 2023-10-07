04bf67e5b5c8fd9b10204aa8c89fd9441acbc7fe
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-07 07:49:47 +0200

    Added OnItemDecorationsRendered context

---
+++ common/src/main/java/com/mlib/contexts/OnItemDecorationsRendered.java
@@ -0,0 +1,29 @@
+package com.mlib.contexts;
+
+import com.mlib.contexts.base.Context;
+import com.mlib.contexts.base.Contexts;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.function.Consumer;
+
+public class OnItemDecorationsRendered {
+	public final GuiGraphics gui;
+	public final Font font;
+	public final ItemStack itemStack;
+	public final int x;
+	public final int y;
+
+	public static Context< OnItemDecorationsRendered > listen( Consumer< OnItemDecorationsRendered > consumer ) {
+		return Contexts.get( OnItemDecorationsRendered.class ).add( consumer );
+	}
+
+	public OnItemDecorationsRendered( GuiGraphics gui, Font font, ItemStack itemStack, int x, int y ) {
+		this.gui = gui;
+		this.font = font;
+		this.itemStack = itemStack;
+		this.x = x;
+		this.y = y;
+	}
+}
--- common/src/main/java/com/mlib/temp/ReplacePotions.java
+++ common/src/main/java/com/mlib/temp/ReplacePotions.java
@@ -1,7 +1,10 @@
 package com.mlib.temp;
 
+import com.mlib.MajruszLibrary;
 import com.mlib.annotation.AutoInstance;
 import com.mlib.contexts.OnItemBrewed;
+import com.mlib.contexts.OnItemDecorationsRendered;
+import com.mlib.contexts.base.Condition;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
@@ -11,5 +14,14 @@ public class ReplacePotions {
 		OnItemBrewed.listen( data->data.mapPotions( itemStacks->itemStacks.stream().map( itemStack->{
 			return itemStack.isEmpty() ? itemStack : new ItemStack( Items.APPLE );
 		} ).toList() ) );
+
+		MajruszLibrary.SIDE.runOnClient( ()->()->{
+			OnItemDecorationsRendered.listen( this::renderDecorations )
+				.addCondition( Condition.predicate( data->data.itemStack.is( Items.APPLE ) ) );
+		} );
+	}
+
+	private void renderDecorations( OnItemDecorationsRendered data ) {
+		data.gui.renderItem( new ItemStack( MajruszLibrary.THE_ITEM.get() ), data.x, data.y );
 	}
 }
