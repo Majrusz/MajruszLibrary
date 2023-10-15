9628bb0b543b7e5b41b0cd8926d3c88c6ce77009
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-15 20:29:52 +0200

    Added class for handling logical safe code

---
+++ common/src/main/java/com/mlib/platform/LogicalSafe.java
@@ -0,0 +1,39 @@
+package com.mlib.platform;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class LogicalSafe< Type > implements Supplier< Type > {
+	private Type client;
+	private Type server;
+
+	public LogicalSafe( Type client, Type server ) {
+		this.client = client;
+		this.server = server;
+	}
+
+	@Override
+	public Type get() {
+		if( Side.isLogicalClient() ) {
+			return this.client;
+		} else {
+			return this.server;
+		}
+	}
+
+	public void set( Type value ) {
+		if( Side.isLogicalClient() ) {
+			this.client = value;
+		} else {
+			this.server = value;
+		}
+	}
+
+	public void set( Function< Type, Type > function ) {
+		if( Side.isLogicalClient() ) {
+			this.client = function.apply( this.client );
+		} else {
+			this.server = function.apply( this.server );
+		}
+	}
+}
