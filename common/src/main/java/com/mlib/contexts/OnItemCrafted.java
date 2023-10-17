4cb935356c2d11c25cd15f8b0a1f002b626f2567
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-17 20:52:55 +0200

    Fixed bug with OnItemCrafted not working properly on quick crafts

--- common/src/main/java/com/mlib/contexts/OnItemCrafted.java
+++ common/src/main/java/com/mlib/contexts/OnItemCrafted.java
@@ -5,24 +5,21 @@ import com.mlib.contexts.base.Contexts;
 import com.mlib.contexts.data.IEntityData;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.function.Consumer;
 
 public class OnItemCrafted implements IEntityData {
 	public final Player player;
-	public final ItemStack crafted;
-	public final CraftingContainer container;
+	public final ItemStack itemStack;
 
 	public static Context< OnItemCrafted > listen( Consumer< OnItemCrafted > consumer ) {
 		return Contexts.get( OnItemCrafted.class ).add( consumer );
 	}
 
-	public OnItemCrafted( Player player, ItemStack crafted, CraftingContainer container ) {
+	public OnItemCrafted( Player player, ItemStack itemStack ) {
 		this.player = player;
-		this.crafted = crafted;
-		this.container = container;
+		this.itemStack = itemStack;
 	}
 
 	@Override
