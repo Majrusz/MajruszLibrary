a3a45a5729db6a5e35e4da9e49d28d3b257b4c2b
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-18 23:13:11 +0200

    Added OnGameInitialized context

--- common/src/main/java/com/mlib/mixin/MixinMinecraft.java
+++ common/src/main/java/com/mlib/mixin/MixinMinecraft.java
@@ -1,6 +1,7 @@
 package com.mlib.mixin;
 
 import com.mlib.contexts.OnClientTicked;
+import com.mlib.contexts.OnGameInitialized;
 import com.mlib.contexts.base.Contexts;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +11,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin( Minecraft.class )
 public abstract class MixinMinecraft {
+	@Inject(
+		at = @At( "HEAD" ),
+		method = "onGameLoadFinished ()V"
+	)
+	private void onGameLoadFinished( CallbackInfo callback ) {
+		Contexts.dispatch( new OnGameInitialized() );
+	}
+
 	@Inject(
 		at = @At( "TAIL" ),
 		method = "tick ()V"
