750b795d4447c2e9dd5c84174c19a558309d69b2
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-19 21:24:12 +0200

    Added support for multi icons in creative mode tabs

---
+++ common/src/main/java/com/mlib/mixin/MixinCreativeModeTab.java
@@ -0,0 +1,30 @@
+package com.mlib.mixin;
+
+import com.mlib.contexts.OnCreativeModeTabIconGet;
+import com.mlib.contexts.base.Contexts;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin( CreativeModeTab.class )
+public abstract class MixinCreativeModeTab {
+	@Shadow @Final private Component displayName;
+
+	@Inject(
+		at = @At( "RETURN" ),
+		cancellable = true,
+		method = "getIconItem ()Lnet/minecraft/world/item/ItemStack;"
+	)
+	private void getIconItem( CallbackInfoReturnable< ItemStack > callback ) {
+		OnCreativeModeTabIconGet data = Contexts.dispatch( new OnCreativeModeTabIconGet( ( CreativeModeTab )( Object )this, this.displayName, callback.getReturnValue() ) );
+		if( !data.icon.isEmpty() ) {
+			callback.setReturnValue( data.icon );
+		}
+	}
+}
