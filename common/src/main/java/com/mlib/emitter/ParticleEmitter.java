6e53492182839c554ce448842632741b12030d44
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-26 08:42:18 +0200

    Added support for size based emitters

--- common/src/main/java/com/mlib/emitter/ParticleEmitter.java
+++ common/src/main/java/com/mlib/emitter/ParticleEmitter.java
@@ -5,6 +5,7 @@ import com.mlib.math.Random;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.HashMap;
@@ -14,10 +15,11 @@ import java.util.function.Supplier;
 public class ParticleEmitter {
 	static final Properties DEFAULT_PROPERTIES = new Properties( ParticleEmitter.offset( 0.25f ), ParticleEmitter.speed( 0.025f, 0.1f ) );
 	static final Map< Object, Properties > PROPERTIES = new HashMap<>();
-	final SimpleParticleType type;
-	Supplier< Vec3 > offset;
-	Supplier< Float > speed;
-	int count = 1;
+	private final SimpleParticleType type;
+	private Supplier< Vec3 > offset;
+	private Supplier< Float > speed;
+	private int count = 1;
+	private float countMultiplier = 1.0f;
 
 	static {
 		ParticleEmitter.setDefault( ParticleTypes.PORTAL, new Properties( ParticleEmitter.offset( 0.5f ), ParticleEmitter.speed( -1.0f, 0.0f ) ) );
@@ -49,20 +51,22 @@ public class ParticleEmitter {
 	}
 
 	public void emit( ServerLevel level, Vec3 position ) {
+		int count = Math.round( this.count * this.countMultiplier );
 		Vec3 offset = this.offset.get();
-		level.sendParticles( this.type, position.x, position.y, position.z, this.count, offset.x, offset.y, offset.z, this.speed.get() );
+		level.sendParticles( this.type, position.x, position.y, position.z, count, offset.x, offset.y, offset.z, this.speed.get() );
 	}
 
 	public void emitLine( ServerLevel level, Vec3 start, Vec3 end ) {
+		int count = Math.round( this.count * this.countMultiplier );
 		AnyPos direction = AnyPos.from( end ).sub( start );
-		if( this.count == 1 ) {
+		if( count == 1 ) {
 			this.emit( level, AnyPos.from( start ).add( direction.div( 2 ) ).vec3() );
 		}
 
-		for( int idx = 0; idx < this.count; idx++ ) {
+		for( int idx = 0; idx < count; idx++ ) {
 			Vec3 offset = this.offset.get();
-			Vec3 position = AnyPos.from( start ).add( direction.mul( idx / ( this.count - 1.0f ) ) ).vec3();
-			level.sendParticles( this.type, position.x, position.y, position.z, this.count, offset.x, offset.y, offset.z, this.speed.get() );
+			Vec3 position = AnyPos.from( start ).add( direction.mul( idx / ( count - 1.0f ) ) ).vec3();
+			level.sendParticles( this.type, position.x, position.y, position.z, count, offset.x, offset.y, offset.z, this.speed.get() );
 		}
 	}
 
@@ -80,6 +84,15 @@ public class ParticleEmitter {
 
 	public ParticleEmitter count( int count ) {
 		this.count = count;
+
+		return this;
+	}
+
+	public ParticleEmitter sizeBased( Entity entity ) {
+		float width = entity.getBbWidth();
+		float height = entity.getBbHeight();
+		this.offset = ()->AnyPos.from( width, height, width ).mul( 0.5 ).vec3();
+		this.countMultiplier = Math.round( ( 1.0f + width + height ) );
 
 		return this;
 	}
