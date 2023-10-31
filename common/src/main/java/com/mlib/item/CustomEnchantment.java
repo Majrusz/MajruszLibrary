8753dc55b7624b8843c3b61b5b8992ee427d342c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-10-31 07:36:56 +0100

    Added extra parameters for enchantments to disable them

--- common/src/main/java/com/mlib/item/CustomEnchantment.java
+++ common/src/main/java/com/mlib/item/CustomEnchantment.java
@@ -2,16 +2,20 @@ package com.mlib.item;
 
 import com.mlib.mixin.IMixinEnchantment;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class CustomEnchantment extends Enchantment {
 	private boolean isCurse = false;
+	private boolean isEnabled = true;
 	private int maxLevel = 1;
 	private CostFormula minLevelCost = level->10;
 	private CostFormula maxLevelCost = level->50;
+	private Predicate< Enchantment > compatibility = enchantment->true;
 
 	public CustomEnchantment() {
 		super( null, null, null );
@@ -33,6 +37,24 @@ public class CustomEnchantment extends Enchantment {
 	}
 
 	@Override
+	public boolean canEnchant( ItemStack itemStack ) {
+		return this.isEnabled()
+			&& super.canEnchant( itemStack );
+	}
+
+	@Override
+	public boolean isTradeable() {
+		return this.isEnabled()
+			&& super.isTradeable();
+	}
+
+	@Override
+	public boolean isDiscoverable() {
+		return this.isEnabled()
+			&& super.isDiscoverable();
+	}
+
+	@Override
 	public boolean isTreasureOnly() {
 		return this.isCurse;
 	}
@@ -40,6 +62,12 @@ public class CustomEnchantment extends Enchantment {
 	@Override
 	public boolean isCurse() {
 		return this.isCurse;
+	}
+
+	@Override
+	public boolean checkCompatibility( Enchantment enchantment ) {
+		return this.compatibility.test( enchantment )
+			&& super.checkCompatibility( enchantment );
 	}
 
 	public CustomEnchantment slots( List< EquipmentSlot > slots ) {
@@ -84,8 +112,22 @@ public class CustomEnchantment extends Enchantment {
 		return this;
 	}
 
+	public CustomEnchantment compatibility( Predicate< Enchantment > predicate ) {
+		this.compatibility = predicate;
+
+		return this;
+	}
+
+	public void setEnabled( boolean isEnabled ) {
+		this.isEnabled = isEnabled;
+	}
+
+	public boolean isEnabled() {
+		return this.isEnabled;
+	}
+
 	@FunctionalInterface
-	protected interface CostFormula {
+	public interface CostFormula {
 		int getLevelCost( int level );
 	}
 }
