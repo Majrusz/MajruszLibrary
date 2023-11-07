e0831b421b74cb3911271c7a32d3b3de16a87b8c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-07 23:30:25 +0100

    Added function to map map values

--- common/src/main/java/com/mlib/collection/CollectionHelper.java
+++ common/src/main/java/com/mlib/collection/CollectionHelper.java
@@ -1,6 +1,8 @@
 package com.mlib.collection;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class CollectionHelper {
@@ -10,5 +12,14 @@ public class CollectionHelper {
 		values.clear();
 
 		return copy;
+	}
+
+	public static < K, V1, V2 > Map< K, V2 > map( Map< K, V1 > input, Function< V1, V2 > mapper, Supplier< Map< K, V2 > > instance ) {
+		Map< K, V2 > output = instance.get();
+		for( K key : input.keySet() ) {
+			output.put( key, mapper.apply( input.get( key ) ) );
+		}
+
+		return output;
 	}
 }
