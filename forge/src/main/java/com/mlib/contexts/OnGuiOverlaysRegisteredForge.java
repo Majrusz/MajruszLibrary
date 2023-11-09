48db7f0766d9a51f6e66e8d24c48d04a87f5d6f9
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-09 20:53:16 +0100

    Added support for rendering gui overlays

---
+++ forge/src/main/java/com/mlib/contexts/OnGuiOverlaysRegisteredForge.java
@@ -0,0 +1,26 @@
+package com.mlib.contexts;
+
+import com.mlib.contexts.base.Contexts;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber( value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD )
+public class OnGuiOverlaysRegisteredForge {
+	@SubscribeEvent
+	public static void registerGui( RegisterGuiOverlaysEvent event ) {
+		for( Pair< String, OnGuiOverlaysRegistered.Renderer > pair : Contexts.dispatch( new OnGuiOverlaysRegistered() ).getRenderers() ) {
+			event.registerBelowAll( pair.getFirst(), new IGuiOverlay() {
+				@Override
+				public void render( ForgeGui gui, GuiGraphics graphics, float partialTick, int screenWidth, int screenHeight ) {
+					pair.getSecond().render( graphics, partialTick, screenWidth, screenHeight );
+				}
+			} );
+		}
+	}
+}
