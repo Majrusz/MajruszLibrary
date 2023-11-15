3e787dee11c2598d2c7ea44b034591803e7b6e01
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-15 22:05:57 +0100

    Fixed server crashes

--- fabric/src/main/java/com/mlib/registry/RegistryFabric.java
+++ fabric/src/main/java/com/mlib/registry/RegistryFabric.java
@@ -45,8 +45,8 @@ public class RegistryFabric implements IRegistryPlatform {
 		callbacks.execute( Custom.SpawnPlacements.class, this::registerSpawnPlacement );
 
 		Side.runOnClient( ()->()->{
-			callbacks.execute( Custom.SpawnPlacements.class, this::registerSpawnPlacement );
 			callbacks.execute( Custom.ModelLayers.class, this::registerModelLayer );
+			callbacks.execute( Custom.Renderers.class, this::registerRenderer );
 		} );
 	}
 
