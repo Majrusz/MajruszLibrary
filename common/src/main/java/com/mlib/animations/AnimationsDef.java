74e51173e567b9fe7bd18f35a6abb613baf58d71
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-18 13:32:16 +0100

    Added support for reading animation positions and scales

--- common/src/main/java/com/mlib/animations/AnimationsDef.java
+++ common/src/main/java/com/mlib/animations/AnimationsDef.java
@@ -23,10 +23,12 @@ public class AnimationsDef {
 			.defineCustomMap( "bones", s->s.bones, ( s, v )->s.bones = v, BoneDef::new );
 
 		Serializables.get( BoneDef.class )
-			.defineCustomMap( "rotation", BoneDef::getRotations, BoneDef::setRotations, RotationDef::new );
+			.defineCustomMap( "rotation", BoneDef::getRotations, BoneDef::setRotations, VectorDef::new )
+			.defineCustomMap( "position", BoneDef::getPositions, BoneDef::setPositions, VectorDef::new )
+			.defineCustomMap( "scale", BoneDef::getScales, BoneDef::setScales, VectorDef::new );
 
-		Serializables.get( RotationDef.class )
-			.defineFloatList( "vector", s->Animation.toRadians3d( s.rotation ), ( s, v )->s.rotation = Animation.toRadians3d( v ) )
+		Serializables.get( VectorDef.class )
+			.defineFloatList( "vector", s->Animation.toRadians3d( s.vector ), ( s, v )->s.vector = Animation.toRadians3d( v ) )
 			.defineEnum( "easing", s->s.easing, ( s, v )->s.easing = v, Easing::values );
 	}
 
@@ -37,19 +39,37 @@ public class AnimationsDef {
 	}
 
 	public static class BoneDef {
-		public TreeMap< Float, RotationDef > rotations = new TreeMap<>();
+		public TreeMap< Float, VectorDef > rotations = new TreeMap<>();
+		public TreeMap< Float, VectorDef > positions = new TreeMap<>();
+		public TreeMap< Float, VectorDef > scales = new TreeMap<>();
 
-		Map< String, RotationDef > getRotations() {
+		void setRotations( Map< String, VectorDef > rotations ) {
+			this.rotations = CollectionHelper.mapKey( rotations, Float::parseFloat, TreeMap::new );
+		}
+
+		void setPositions( Map< String, VectorDef > rotations ) {
+			this.positions = CollectionHelper.mapKey( rotations, Float::parseFloat, TreeMap::new );
+		}
+
+		void setScales( Map< String, VectorDef > rotations ) {
+			this.scales = CollectionHelper.mapKey( rotations, Float::parseFloat, TreeMap::new );
+		}
+
+		Map< String, VectorDef > getRotations() {
 			return CollectionHelper.mapKey( this.rotations, Object::toString, HashMap::new );
 		}
 
-		void setRotations( Map< String, RotationDef > rotations ) {
-			this.rotations = CollectionHelper.mapKey( rotations, Float::parseFloat, TreeMap::new );
+		Map< String, VectorDef > getPositions() {
+			return CollectionHelper.mapKey( this.positions, Object::toString, HashMap::new );
+		}
+
+		Map< String, VectorDef > getScales() {
+			return CollectionHelper.mapKey( this.scales, Object::toString, HashMap::new );
 		}
 	}
 
-	public static class RotationDef {
-		public Vector3f rotation = new Vector3f( 0.0f, 0.0f, 0.0f );
+	public static class VectorDef {
+		public Vector3f vector = new Vector3f( 0.0f, 0.0f, 0.0f );
 		public Easing easing = Easing.LINEAR;
 	}
 
