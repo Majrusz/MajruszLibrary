c92242b8887d02487bf0d645b92561f88dcdcea8
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-22 16:36:22 +0100

    Added builder for chest loot params

--- common/src/main/java/com/mlib/item/LootHelper.java
+++ common/src/main/java/com/mlib/item/LootHelper.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.Vec3;
 
 public class LootHelper {
 	public static LootTable getLootTable( ResourceLocation id ) {
@@ -23,5 +24,11 @@ public class LootHelper {
 			.withParameter( LootContextParams.THIS_ENTITY, entity )
 			.withLuck( entity instanceof Player player ? player.getLuck() : 0.0f )
 			.create( LootContextParamSets.GIFT );
+	}
+
+	public static LootParams toChestParams( ServerLevel level, Vec3 origin ) {
+		return new LootParams.Builder( level )
+			.withParameter( LootContextParams.ORIGIN, origin )
+			.create( LootContextParamSets.CHEST );
 	}
 }
