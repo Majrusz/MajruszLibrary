22886b33492e61e60397df2541908a68d70fb497
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-25 22:53:56 +0100

    Changed effect parameter name

--- common/src/main/java/com/majruszlibrary/entity/EffectDef.java
+++ common/src/main/java/com/majruszlibrary/entity/EffectDef.java
@@ -19,7 +19,7 @@ public class EffectDef {
 
 	static {
 		Serializables.get( EffectDef.class )
-			.define( "effect", Reader.optional( Reader.mobEffect() ), s->s.effect.get(), ( s, v )->s.effect = ()->v )
+			.define( "id", Reader.optional( Reader.mobEffect() ), s->s.effect.get(), ( s, v )->s.effect = ()->v )
 			.define( "amplifier", Reader.integer(), s->s.amplifier, ( s, v )->s.amplifier = Range.of( 0, 10 ).clamp( v ) )
 			.define( "duration", Reader.number(), s->s.duration, ( s, v )->s.duration = Range.of( 1.0f, 1000.0f ).clamp( v ) );
 	}
