25a7455afcd659567dc4aff4420b2238859533c9
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-27 16:53:36 +0100

    Added OnItemRenderColorGet event

--- common/src/main/java/com/majruszlibrary/events/OnItemEntityRendered.java
+++ common/src/main/java/com/majruszlibrary/events/OnItemEntityRendered.java
@@ -4,9 +4,7 @@ import com.majruszlibrary.annotation.Dist;
 import com.majruszlibrary.annotation.OnlyIn;
 import com.majruszlibrary.events.base.Event;
 import com.majruszlibrary.events.base.Events;
-import com.majruszlibrary.platform.Side;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
@@ -15,7 +13,6 @@ import java.util.function.Consumer;
 
 @OnlyIn( Dist.CLIENT )
 public class OnItemEntityRendered {
-	public final LocalPlayer player;
 	public final ItemStack itemStack;
 	public final ItemDisplayContext context;
 	public final PoseStack poseStack;
@@ -28,7 +25,6 @@ public class OnItemEntityRendered {
 	}
 
 	public OnItemEntityRendered( ItemStack itemStack, ItemDisplayContext context, PoseStack poseStack, MultiBufferSource multiBufferSource, int x, int y ) {
-		this.player = Side.getLocalPlayer();
 		this.itemStack = itemStack;
 		this.context = context;
 		this.poseStack = poseStack;
---
+++ common/src/main/java/com/majruszlibrary/events/OnItemRenderColorGet.java
@@ -0,0 +1,29 @@
+package com.majruszlibrary.events;
+
+import com.majruszlibrary.events.base.Event;
+import com.majruszlibrary.events.base.Events;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.function.Consumer;
+
+public class OnItemRenderColorGet {
+	public final ItemStack itemStack;
+	public final int layerIdx;
+	public final int original;
+	public int color;
+
+	public static Event< OnItemRenderColorGet > listen( Consumer< OnItemRenderColorGet > consumer ) {
+		return Events.get( OnItemRenderColorGet.class ).add( consumer );
+	}
+
+	public OnItemRenderColorGet( ItemStack itemStack, int layerIdx, int color ) {
+		this.itemStack = itemStack;
+		this.layerIdx = layerIdx;
+		this.original = color;
+		this.color = color;
+	}
+
+	public int getColor() {
+		return this.color;
+	}
+}
